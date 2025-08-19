6fcd488fc3b05407df0ec607dc166956d2f2f59d
gitXite <daniel.halaas@gmail.com>
2025-08-19 14:42:03 +0200

    Created an About component

---
+++ src/components/About.tsx
@@ -0,0 +1,20 @@
+
+
+
+function About() {
+    return (
+        <div className='flex h-full w-full bg-neutral-100 place-content-center justify-around'>
+            <div className='flex flex-col place-content-start place-items-center w-1/3 mt-20'>
+                <h1 className='mb-10 text-6xl text-neutral-900'>Placeholder</h1>
+                <p className='w-8/12 text-center text-neutral-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima dolore, consequuntur alias quasi voluptate corporis ducimus asperiores! Commodi quasi repudiandae impedit facilis expedita cupiditate numquam ratione facere fuga. Eos, corporis.</p>
+            </div>
+            <div className='flex flex-col place-content-end place-items-center w-1/3 mb-20'>
+                <h1 className='mb-10 text-6xl text-neutral-900'>Placeholder</h1>
+                <p className='w-8/12 text-center text-neutral-700 '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque ad, exercitationem quisquam ducimus numquam iure quaerat, incidunt deserunt expedita tempore at delectus quasi molestias necessitatibus sit est reprehenderit odit ratione!</p>
+            </div>
+        </div>
+    );
+}
+
+
+export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import MapSelector from '../components/MapSelector';
 import Welcome from '../components/Welcome';
+import About from '../components/About';
 
 
 function Home() {
     return (
         <div className='h-full w-full bg-neutral-100'>
             <Welcome />
+            <About />
             <MapSelector />
         </div>
     );
