3b55d0a09e2b9b2bd0b59ee69fbfc5d3191177ad
gitXite <daniel.halaas@gmail.com>
2025-08-19 17:29:56 +0200

    Added animation to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,34 @@
-
+import { useState, useEffect } from 'react';
 
 
 function Header() {
+    const [showHeader, setShowHeader] = useState(false)
+
+    useEffect(() => {
+        let timeout: ReturnType<typeof setTimeout>;
+        const handleScroll = () => {
+            setShowHeader(true);
+
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+                setShowHeader(false);
+            }, 1000);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+            clearTimeout(timeout);
+        }
+    }, []);
+
+    
     return (
-        <header className='flex h-1/5 w-full sticky'>
-            <div></div>
+        <header className={`flex h-auto w-full p-5 fixed bg-neutral-500/10 z-1 drop-shadow-2xl place-content-center backdrop-blur-sm border-b-1 border-neutral-300 transition-transform duration-300 ${
+            showHeader ? 'translate-y-0' : '-translate-y-full'
+            }`}>
+            <div className='text-4xl text-neutral-900'>MD // DESIGN</div>
         </header>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,13 @@ import MapSelector from '../components/MapSelector';
 import Welcome from '../components/Welcome';
 import About from '../components/About';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 
 function Home() {
     return (
         <div className='h-full w-full bg-neutral-100'>
+            <Header />
             <Welcome />
             <About />
             <MapSelector />
