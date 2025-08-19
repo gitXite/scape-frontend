c1c31a1ee854463ddb770e9a6472052bf6d08c69
gitXite <daniel.halaas@gmail.com>
2025-08-19 14:20:52 +0200

    Restructured Home page and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-
-import MapSelector from './components/MapSelector';
 import Home from './pages/Home';
+
 
 function App() {
     return (
-        <div className='h-full w-full bg-neutral-100'>
-            <Home />
-            <MapSelector />
-        </div>
+        <Home />
     );
 }
 
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,18 @@
+
+
+
+function Welcome() {
+    return (
+        <div className='h-full w-full place-content-center'>
+            <div className='flex flex-col place-items-center place-content-center h-8/12 w-full bg-neutral-900 drop-shadow-2xl drop-shadow-neutral-500'>
+                <h1 className='mt-10 text-9xl'>W  E   L   C   O   M   E</h1>
+                <button className='flex mt-20 p-8 pl-15 pr-15 text-xl border-1 border-neutral-100 rounded-md hover:bg-neutral-100 hover:text-neutral-900 hover:cursor-pointer'>
+                    <p>G   e   t</p><span className='w-4'></span><p>S   t   a   r   t   e   d</p>
+                </button>
+            </div>
+        </div>
+    );
+}
+
+
+export default Welcome;
