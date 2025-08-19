827916d580676317f58507ba5dfb09002bf4d8bc
gitXite <daniel.halaas@gmail.com>
2025-08-19 14:15:44 +0200

    Tried to add color theme to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ const withMT = require('@material-tailwind/react/utils/withMT');
 module.exports = withMT({
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                peach: '#f7c59f',
+                spaceCadet: '#2a324b',
+                slateGray: '#767b91',
+                frenchGray: '#c7ccdb',
+                aliceBlue: '#e1e5ee',
+            },
+        },
     },
     plugins: [],
 });
