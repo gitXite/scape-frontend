095ed814a81f459796acde79edbf3afd42ca393a
gitXite <daniel.halaas@gmail.com>
2025-08-20 10:46:51 +0200

    Added Stripe logo to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,11 @@ function Footer() {
         <footer className='flex justify-center place-items-end h-50 w-full p-5 border-t-1 bg-neutral-900'>
             <div className='flex-col place-items-center'>
                 <div className='h-fit w-auto text-2xl text-neutral-100 p-2'>MD // DESIGN</div>
-                <small className='text-neutral-100'>Copyright © {new Date().getFullYear()} by Daniel Halås. All Rights Reserved.</small>
+                <small className=''>Copyright © {new Date().getFullYear()} by Daniel Halås. All Rights Reserved.</small>
+            </div>
+            <div className='flex ml-5 absolute left-0 place-items-end'>
+                <small>Powered by</small>
+                <img className='w-2/10 m-0 p-0 relative -bottom-3' src="src/assets/Stripe_Logo.png" alt="Stripe Logo" />
             </div>
         </footer>
     );
