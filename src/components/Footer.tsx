6679cf7b638823fef928d90649e98624a2281490
gitXite <daniel.halaas@gmail.com>
2025-08-20 08:55:09 +0200

    Brand in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,11 @@
 
 function Footer() {
     return (
-        <footer className='flex justify-center place-items-center h-50 w-full p-10 border-t-1 bg-neutral-900'>
-            <small className='text-neutral-100 mt-40'>Copyright © {new Date().getFullYear()} by Daniel Halås. All Rights Reserved.</small>
+        <footer className='flex justify-center place-items-end h-50 w-full p-5 border-t-1 bg-neutral-900'>
+            <div className='flex-col place-items-center'>
+                <div className='h-fit w-auto text-2xl text-neutral-100 p-2'>MD // DESIGN</div>
+                <small className='text-neutral-100'>Copyright © {new Date().getFullYear()} by Daniel Halås. All Rights Reserved.</small>
+            </div>
         </footer>
     );
 }
