35eaf5775a97b75618d39036f104d09bac9e0264
gitXite <daniel.halaas@gmail.com>
2025-08-20 08:25:41 +0200

    Added hover state for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,27 @@ import { useState, useEffect } from 'react';
 
 function Header() {
     const [showHeader, setShowHeader] = useState(false)
+    const [ishovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    }
 
     useEffect(() => {
         let timeout: ReturnType<typeof setTimeout>;
+
         const handleScroll = () => {
             setShowHeader(true);
 
             clearTimeout(timeout)
-            timeout = setTimeout(() => {
-                setShowHeader(false);
-            }, 1000);
+            if (!ishovered) {
+                timeout = setTimeout(() => {
+                    setShowHeader(false);
+                }, 1000);
+            }
         };
 
         window.addEventListener("scroll", handleScroll);
@@ -25,10 +36,10 @@ function Header() {
 
 
     return (
-        <header className={`flex h-auto w-full p-5 fixed bg-neutral-500/10 z-1 drop-shadow-2xl place-content-center backdrop-blur-sm border-b-1 border-neutral-300 transition-transform duration-300 ${
-            showHeader ? 'translate-y-0' : '-translate-y-full'
+        <header onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`flex h-auto w-full p-5 fixed mix-blend-difference bg-neutral-500/10 z-1 drop-shadow-2xl place-content-center backdrop-blur-sm border-b-1 border-neutral-300 transition-transform duration-300 ${
+            showHeader || ishovered ? 'translate-y-0' : '-translate-y-full'
             }`}>
-            <div className='text-4xl text-neutral-900'>MD // DESIGN</div>
+            <div className='text-4xl text-neutral-100 mix-blend-difference'>MD // DESIGN</div>
         </header>
     );
 }
