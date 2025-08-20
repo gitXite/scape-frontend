bdaf743cabcd92bc47195f9be058f7c29ddd55f2
gitXite <daniel.halaas@gmail.com>
2025-08-20 12:02:18 +0200

    Footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className='flex justify-center place-items-end h-50 w-full p-5 border-t-1 bg-neutral-900'>
             <div className='flex-col place-items-center'>
-                <div className='h-fit w-auto text-2xl text-neutral-100 p-2'>MD // DESIGN</div>
+                <div className='h-fit w-auto text-2xl text-neutral-100 p-2 pb-20'>MD // DESIGN</div>
                 <small className=''>Copyright © {new Date().getFullYear()} by Daniel Halås. All Rights Reserved.</small>
             </div>
             <div className='flex ml-5 absolute left-0 place-items-end'>
