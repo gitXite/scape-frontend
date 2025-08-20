f8ae11aa64987848106f99f7f188fe95f9c513cd
gitXite <daniel.halaas@gmail.com>
2025-08-20 09:54:12 +0200

    Doubled the height of the about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,32 +8,64 @@ const options = {
 function About() {
     const { ref: ref1, inView: inView1 } = useInView(options);
     const { ref: ref2, inView: inView2 } = useInView(options);
+    const { ref: ref3, inView: inView3 } = useInView(options);
+    const { ref: ref4, inView: inView4 } = useInView(options);
+
+
 
     return (
-        <div ref={ref1} className='flex h-full w-full bg-neutral-100 place-content-center justify-around'>
-            <div className={`flex flex-col h-fit place-items-center w-1/3 mb-80 transition-opacity duration-600 ease-in ${
-                inView1 ? 'opacity-100' : 'opacity-0'
-            }`}>
-                <h1 className='mb-10 text-6xl text-neutral-900'>Placeholder</h1>
-                <p className='w-8/12 text-center text-neutral-700 leading-8'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Minima dolore, consequuntur alias quasi voluptate corporis
-                    ducimus asperiores! Commodi quasi repudiandae impedit
-                    facilis expedita cupiditate numquam ratione facere fuga.
-                    Eos, corporis.
-                </p>
+        <div className='flex h-[200vh] w-full bg-neutral-100  justify-around'>
+            <div className='flex-col w-1/3 place-content-center'>
+                <div ref={ref1} className={`flex flex-col h-fit place-items-center mb-100 transition-opacity duration-600 ease-in ${
+                    inView1 ? 'opacity-100' : 'opacity-0'
+                }`}>
+                    <h1 className='mb-10 text-6xl text-neutral-900'>Placeholder</h1>
+                    <p className='w-8/12 text-center text-neutral-700 leading-8'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Minima dolore, consequuntur alias quasi voluptate corporis
+                        ducimus asperiores! Commodi quasi repudiandae impedit
+                        facilis expedita cupiditate numquam ratione facere fuga.
+                        Eos, corporis.
+                    </p>
+                </div>
+                <div ref={ref2} className={`flex flex-col h-fit place-items-center mb-120 transition-opacity duration-600 ease-in ${
+                    inView2 ? 'opacity-100' : 'opacity-0'
+                }`}>
+                    <h1 className='mb-10 text-6xl text-neutral-900'>Placeholder</h1>
+                    <p className='w-8/12 text-center text-neutral-700 leading-8'>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Doloremque ad, exercitationem quisquam ducimus numquam iure
+                        quaerat, incidunt deserunt expedita tempore at delectus
+                        quasi molestias necessitatibus sit est reprehenderit odit
+                        ratione!
+                    </p>
+                </div>
             </div>
-            <div ref={ref2} className={`flex flex-col h-fit place-items-center w-1/3 mt-100 transition-opacity duration-600 ease-in ${
-                inView2 ? 'opacity-100' : 'opacity-0'
-            }`}>
-                <h1 className='mb-10 text-6xl text-neutral-900'>Placeholder</h1>
-                <p className='w-8/12 text-center text-neutral-700 leading-8'>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Doloremque ad, exercitationem quisquam ducimus numquam iure
-                    quaerat, incidunt deserunt expedita tempore at delectus
-                    quasi molestias necessitatibus sit est reprehenderit odit
-                    ratione!
-                </p>
+            <div className='flex-col w-1/3'>
+                <div ref={ref3} className={`flex flex-col h-fit place-items-center mt-120 transition-opacity duration-600 ease-in ${
+                    inView3 ? 'opacity-100' : 'opacity-0'
+                }`}>
+                    <h1 className='mb-10 text-6xl text-neutral-900'>Placeholder</h1>
+                    <p className='w-8/12 text-center text-neutral-700 leading-8'>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Doloremque ad, exercitationem quisquam ducimus numquam iure
+                        quaerat, incidunt deserunt expedita tempore at delectus
+                        quasi molestias necessitatibus sit est reprehenderit odit
+                        ratione!
+                    </p>
+                </div>
+                <div ref={ref4} className={`flex flex-col h-fit place-items-center mt-100 transition-opacity duration-600 ease-in ${
+                    inView4 ? 'opacity-100' : 'opacity-0'
+                }`}>
+                    <h1 className='mb-10 text-6xl text-neutral-900'>Placeholder</h1>
+                    <p className='w-8/12 text-center text-neutral-700 leading-8'>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Doloremque ad, exercitationem quisquam ducimus numquam iure
+                        quaerat, incidunt deserunt expedita tempore at delectus
+                        quasi molestias necessitatibus sit est reprehenderit odit
+                        ratione!
+                    </p>
+                </div>
             </div>
         </div>
     );
