44ea16fba265ba61b7d7dd61c586a8e34426d1f6
Daniel <Daniel.halaas@gmail.com>
2025-08-21 09:40:25 +0200

    Created VideoBackground component for landingpage

---
+++ src/components/VideoBackground.tsx
@@ -0,0 +1,20 @@
+// import video
+
+
+function VideoBackground() {
+    return (
+        <div className='relative h-full w-full overflow-hidden'>
+            <video
+                className='absolute top-0 left-0 h-full w-full object-cover'
+                src=''
+                autoPlay
+                muted
+                loop
+                playsInline
+            />
+        </div>
+    );
+}
+
+
+export default VideoBackground;
