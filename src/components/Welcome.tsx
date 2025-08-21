2afcf0620c1baf81002efd75a460397e468635e0
gitXite <daniel.halaas@gmail.com>
2025-08-21 15:47:37 +0200

    Small styling change

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -6,8 +6,8 @@ function Welcome() {
         <div id='welcome' className='h-full w-full place-content-center'>
             <div className='flex flex-col place-items-center place-content-center h-8/12 w-full shadow-[inset_0px_0px_20px_rgba(0,0,0,0.6)] bg-neutral-900 drop-shadow-2xl drop-shadow-neutral-500'>
                 <VideoBackground />
-                <h1 className='z-2 mt-10 text-9xl drop-shadow-black drop-shadow-2xl'>W  E   L   C   O   M   E</h1>
-                <button className='z-2 flex mt-20 p-6 pl-12 pr-12 text-xl text-neutral-100 border-1 shadow-[inset_0px_0px_10px_rgba(0,0,0,0.6)] hover:shadow-[inset_0px_0px_15px_rgba(0,0,0,0.6)] active:shadow-[inset_0px_0px_10px_rgba(0,0,0,0.6)] shadow-neutral-950 border-neutral-100 rounded-md .hover:bg-neutral-100 .hover:text-neutral-900 .hover:border-neutral-900 hover:cursor-pointer'>
+                <h1 className='z-2 mt-10 text-9xl drop-shadow-black drop-shadow-2xl'>W   E   L   C   O   M   E</h1>
+                <button className='z-2 flex mt-20 p-6 pl-12 pr-12 text-xl backdrop-blur-md text-neutral-100 border-1 shadow-[inset_0px_0px_10px_rgba(0,0,0,0.6)] hover:shadow-[inset_0px_0px_15px_rgba(0,0,0,0.6)] active:shadow-[inset_0px_0px_10px_rgba(0,0,0,0.6)] shadow-neutral-950 border-neutral-100 rounded-md .hover:bg-neutral-100 .hover:text-neutral-900 .hover:border-neutral-900 hover:cursor-pointer'>
                     <p>G   e   t</p><span className='w-4'></span><p>S   t   a   r   t   e   d</p>
                 </button>
             </div>
