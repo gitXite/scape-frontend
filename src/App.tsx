413e51f908d8e2a32cd3140fdcbc8d4c8cc92304
gitXite <daniel.halaas@gmail.com>
2025-08-21 18:28:03 +0200

    Created a Checkout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Home from './pages/Home';
 import GetStarted from './pages/GetStarted';
+import Checkout from './pages/Checkout';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/get-started' element={<GetStarted />} />
+                <Route path='/checkout' element={<Checkout />} />
             </Routes>
         </Router>
     );
---
+++ src/pages/Checkout.tsx
@@ -0,0 +1,12 @@
+import Footer from '../components/Footer';
+
+
+
+function Checkout() {
+    return (
+        <Footer />
+    );
+}
+
+
+export default Checkout;
