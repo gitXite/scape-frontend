7e75b7feb2f8fadafaa8cf9ae1d0cf522770c0bc
gitXite <daniel.halaas@gmail.com>
2025-08-21 15:36:14 +0200

    Fixed Map padding

--- src/components/MapSelector.tsx
+++ src/components/MapSelector.tsx
@@ -90,7 +90,7 @@ function MapSelector() {
     
 
     return (
-        <div id='map' className='flex flex-col h-full w-full bg-neutral-100 place-content-start items-center pt-15'>
+        <div id='map' className='flex flex-col h-full w-full bg-neutral-100 place-content-start items-center pt-20'>
             <div ref={ref} className={`flex flex-col h-full w-full place-content-start items-center transition-opacity duration-600 ease-in ${
                 inView ? 'opacity-100' : 'opacity-0'
             }`}>
