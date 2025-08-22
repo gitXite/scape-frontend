0f3820cc4f417bb06da6924c2eb175a2104a5c14
Daniel <Daniel.halaas@gmail.com>
2025-08-22 17:44:54 +0200

    Styling changes and fixes

--- src/components/CheckoutSummary.tsx
+++ src/components/CheckoutSummary.tsx
@@ -9,33 +9,33 @@ function CheckoutSummary() {
             </div>
             <div className='flex-col h-full w-2/4 p-10'>
                 <div className='flex-col pb-5 border-b-1 border-neutral-900'>
-                    <h1>Order Summary</h1>
-                    <div className='flex'>
+                    <h1 className='pb-5'>Order Summary</h1>
+                    <div className='flex justify-between'>
                         <img src='' alt='' className='h-20 w-20 rounded-sm' />
                         <h2 className='text-neutral-900'>Model</h2>
                         <p className='text-neutral-600'>199kr</p>
                     </div>
-                    <div className='flex'>
+                    <div className='flex justify-between'>
                         <img src='' alt='' className='h-20 w-20 rounded-sm' />
                         <h2 className='text-neutral-900'>Frame</h2>
                         <p className='text-neutral-600'>99kr</p>
                     </div>
                 </div>
-                <div className='flex-col pb-5 border-b-1 border-neutral-900'>
-                    <div className='flex place-content-between'>
+                <div className='flex-col pt-5 pb-5 border-b-1 border-neutral-900'>
+                    <div className='flex justify-between'>
                         <p className='text-neutral-900'>Subtotal</p>
                         <p className='text-neutral-600'>298kr</p>
                     </div>
-                    <div className='flex place-content-between'>
+                    <div className='flex justify-between'>
                         <p className='text-neutral-900'>Tax Included</p>
                         <p className='text-neutral-600'>74.5kr</p>
                     </div>
-                    <div className='flex place-content-between'>
+                    <div className='flex justify-between'>
                         <p className='text-neutral-900'>Shipping</p>
                         <p className='text-neutral-600'>49kr</p>
                     </div>
                 </div>
-                <div className='flex place-content-between'>
+                <div className='flex justify-between'>
                     <h1 className='text-neutral-900'>Total</h1>
                     <p className='text-neutral-900'>347kr</p>
                 </div>
