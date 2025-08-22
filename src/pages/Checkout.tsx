44125514f874be23fd9977f624367de24c5c723d
Daniel <Daniel.halaas@gmail.com>
2025-08-22 17:18:44 +0200

    Update Checkout page to incorporate CheckoutSummary component

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,10 +1,13 @@
 import Footer from '../components/Footer';
-
+import CheckoutSummary from '../components/CheckoutSummary';
 
 
 function Checkout() {
     return (
-        <Footer />
+        <div className='flex-col h-full w-full bg-neutral-100'>
+            <CheckoutSummary />
+            <Footer />
+        </div>
     );
 }
 
