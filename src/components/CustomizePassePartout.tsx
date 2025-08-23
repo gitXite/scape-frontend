1561594c8d2aa0d4ef52033b35c454c5ae62b1e5
Daniel <Daniel.halaas@gmail.com>
2025-08-23 15:15:35 +0200

    Created CustomizePassePartout.tsx component

---
+++ src/components/CustomizePassePartout.tsx
@@ -0,0 +1,26 @@
+
+
+
+function CustomizePassePartout() {
+    return (
+        <div className='flex h-full w-full'>
+            <div className='flex h-full w-1/3'>
+                <div className='flex-col justify-center'>
+                    <img src='' alt='Without' className='bg-neutral-900' />
+                    <p>Without</p>
+                </div>
+                <div className='flex-col justify-center'>
+                    <img src='' alt='White' className='bg-neutral-900' />
+                    <p>White</p>
+                </div>
+                <div className='flex-col justify-center'>
+                    <img src='' alt='Black' className='bg-neutral-900' />
+                    <p>Black</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default CustomizePassePartout;
