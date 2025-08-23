698013f282435aff442f8fda10fd0fe28b3a10c4
Daniel <Daniel.halaas@gmail.com>
2025-08-23 10:19:07 +0200

    Restructured components

---
+++ src/components/CheckoutPayment.tsx
@@ -0,0 +1,13 @@
+/* integrate Stripe API */
+
+
+function CheckoutPayment() {
+    return (
+        <div className='h-full w-2/4 p-10'>
+            {/* Stripe payment */}
+        </div>
+    );
+}
+
+
+export default CheckoutPayment;
