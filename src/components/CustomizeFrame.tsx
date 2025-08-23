bc88c2e85ed60779206014ac0497d9e694a44aaf
Daniel <Daniel.halaas@gmail.com>
2025-08-23 15:17:23 +0200

    Update CustomizeFrame.tsx

--- src/components/CustomizeFrame.tsx
+++ src/components/CustomizeFrame.tsx
@@ -4,11 +4,6 @@
 function CustomizeFrame() {
     return (
         <div className='flex h-full w-full'>
-            <div className='h-full w-2/3 place-content-center place-items-center'>
-                <div className='h-[80%] p-10'>
-                    <img src='' alt='Preview' className='bg-neutral-900' />
-                </div>
-            </div>
             <div className='flex h-full w-1/3'>
                 <div className='flex-col justify-center'>
                     <img src='' alt='Oak Frame' className='bg-neutral-900' />
