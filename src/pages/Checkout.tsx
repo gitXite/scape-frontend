5b1abe7700ae296677809ffc94f746c2a5c89dff
Daniel <Daniel.halaas@gmail.com>
2025-08-23 10:23:52 +0200

    Restructured components

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,11 +1,15 @@
 import Footer from '../components/Footer';
 import CheckoutSummary from '../components/CheckoutSummary';
+import CheckoutPayment from '../components/CheckoutPayment';
 
 
 function Checkout() {
     return (
         <div className='flex-col h-full w-full bg-neutral-100'>
-            <CheckoutSummary />
+            <div className='flex h-full w-full'>
+                <CheckoutPayment />
+                <CheckoutSummary />
+            </div>
             <Footer />
         </div>
     );
