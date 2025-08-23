894ec2bb1118820c6f713b88ec39f7a4b46f0982
Daniel <Daniel.halaas@gmail.com>
2025-08-23 15:17:00 +0200

    Created ModelPreview.tsx component

---
+++ src/components/ModelPreview.tsx
@@ -0,0 +1,15 @@
+
+
+
+function ModelPreview() {
+    return (
+        <div className='h-full w-2/3 place-content-center place-items-center'>
+            <div className='h-[80%] p-10'>
+                <img src='' alt='Preview' className='bg-neutral-900' />
+            </div>
+        </div>
+    );
+}
+
+
+export default ModelPreview;
