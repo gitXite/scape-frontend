b83c41fac9e387dfa7d3239a3eabb7077ba18afb
gitXite <daniel.halaas@gmail.com>
2025-08-25 14:45:13 +0200

    Added Separator ui component

--- src/components/CheckoutSummary.tsx
+++ src/components/CheckoutSummary.tsx
@@ -1,23 +1,26 @@
+import { Separator } from './ui/separator';
 
 
 
 function CheckoutSummary() {
     return (
-        <div className='flex-col h-full w-2/4 p-10'>
-            <div className='flex-col pb-5 border-b-1 border-neutral-900'>
-                <h1 className='pb-5'>Order Summary</h1>
-                <div className='flex justify-between'>
-                    <img src='' alt='' className='h-20 w-20 rounded-sm' />
+        <div className='flex-col h-full w-2/4 p-10 content-center'>
+            <div className='flex-col pb-5'>
+                <h1 className='pb-5 text-neutral-900'>Order Summary</h1>
+                <Separator orientation='horizontal'/>
+                <div className='flex justify-between py-2'>
+                    <img src='src/assets/product-image-straight.png' alt='Product Image 1' className='h-20 w-30 rounded-sm object-cover' />
                     <h2 className='text-neutral-900'>Model</h2>
                     <p className='text-neutral-600'>199kr</p>
                 </div>
-                <div className='flex justify-between'>
-                    <img src='' alt='' className='h-20 w-20 rounded-sm' />
+                <div className='flex justify-between py-2'>
+                    <img src='src/assets/product-image-angle.png' alt='Product Image 2' className='h-20 w-30 rounded-sm object-cover' />
                     <h2 className='text-neutral-900'>Frame</h2>
                     <p className='text-neutral-600'>99kr</p>
                 </div>
             </div>
-            <div className='flex-col pt-5 pb-5 border-b-1 border-neutral-900'>
+            <Separator orientation='horizontal'/>
+            <div className='flex-col pt-5 pb-5'>
                 <div className='flex justify-between'>
                     <p className='text-neutral-900'>Subtotal</p>
                     <p className='text-neutral-600'>298kr</p>
@@ -31,6 +34,7 @@ function CheckoutSummary() {
                     <p className='text-neutral-600'>49kr</p>
                 </div>
             </div>
+            <Separator orientation='horizontal'/>
             <div className='flex justify-between'>
                 <h1 className='text-neutral-900'>Total</h1>
                 <p className='text-neutral-900'>347kr</p>
--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,13 +1,15 @@
 import Footer from '../components/Footer';
 import CheckoutSummary from '../components/CheckoutSummary';
 import CheckoutPayment from '../components/CheckoutPayment';
+import { Separator } from '@/components/ui/separator';
 
 
 function Checkout() {
     return (
         <div className='flex-col h-full w-full bg-neutral-100'>
-            <div className='flex h-full w-full'>
+            <div className='flex h-full w-full items-center content-center'>
                 <CheckoutPayment />
+                <Separator className=''/>
                 <CheckoutSummary />
             </div>
             <Footer />
