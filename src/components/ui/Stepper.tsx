479c18195b61848a9c8c3024bf2868101264c089
gitXite <daniel.halaas@gmail.com>
2025-08-25 16:08:25 +0200

    Import

--- src/components/ui/Stepper.tsx
+++ src/components/ui/Stepper.tsx
@@ -1,6 +1,6 @@
+import type { JSX } from 'react';
 import { cn } from '@/lib/utils';
 import { RippleButton } from '../animate-ui/buttons/Ripple';
-import type { JSX } from 'react';
 
 interface StepperProps {
     steps: Array<{ component: JSX.Element }>;
