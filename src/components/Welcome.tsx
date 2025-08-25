5af61eb0ea5171a25911a3cbcda728ebf9ddb328
gitXite <daniel.halaas@gmail.com>
2025-08-25 09:28:39 +0200

    Anchor styling

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -7,7 +7,7 @@ function Welcome() {
 
     return (
         <div id='home' className='h-full w-full place-content-center'>
-            <div className='flex flex-col place-items-center place-content-center h-8/12 w-full shadow-[inset_0px_0px_20px_rgba(0,0,0,0.6)] bg-neutral-900 drop-shadow-2xl drop-shadow-neutral-500'>
+            <div className='flex flex-col place-items-center place-content-center  h-8/12 w-full shadow-[inset_0px_0px_20px_rgba(0,0,0,0.6)] bg-neutral-900 drop-shadow-2xl drop-shadow-neutral-500'>
                 <VideoBackground />
                 <h1 className='z-2 mt-10 text-9xl drop-shadow-black drop-shadow-2xl'>W   E   L   C   O   M   E</h1>
                 <button 
@@ -17,7 +17,9 @@ function Welcome() {
                     <p>S   t   a   r   t</p><span className='w-4'></span><p>C   u   s   t   o   m   i   z   i   n   g</p>
                 </button>
             </div>
-            <a href='#about' className='text-lg text-neutral-600 relative top-10 cursor-pointer'>Read More</a>
+            <div className='text-center'>
+                <a href='#about' className='text-lg text-neutral-600 relative top-10 cursor-pointer hover:text-neutral-900 active:text-neutral-600'>Read More</a>
+            </div>
         </div>
     );
 }
