8d099a6e8b89db62d31b15141c7da5033bee47cd
gitXite <daniel.halaas@gmail.com>
2025-08-26 13:27:30 +0200

    Stylling customize page

--- src/components/CustomizeFrame.tsx
+++ src/components/CustomizeFrame.tsx
@@ -13,7 +13,8 @@ function CustomizeFrame() {
                 />
                 <div className='flex flex-col justify-center items-center border rounded-sm p-4 transition peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-sky-300 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
                     <img src='src/assets/product-image-without-pp.png' alt='Oak Frame' className='w-50 rounded my-5 mb-15' />
-                    <p className='text-neutral-900 mt-10'>Oak</p>
+                    <p className='text-neutral-900 text-xl mt-2 font-normal'>Oak</p>
+                    <p className='text-neutral-600 mt-10'>99kr</p>
                 </div>
             </label>
             <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
@@ -25,7 +26,8 @@ function CustomizeFrame() {
                 />
                 <div className='flex flex-col justify-center items-center border rounded-sm p-4 transition peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-sky-300 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
                     <img src='src/assets/product-image-without-pp.png' alt='Walnut Frame' className='w-50 rounded my-5 mb-15' />
-                    <p className='text-neutral-900 mt-10'>Walnut</p>
+                    <p className='text-neutral-900 text-xl mt-2 font-normal'>Walnut</p>
+                    <p className='text-neutral-600 mt-10'>99kr</p>
                 </div>
             </label>
             <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
@@ -37,7 +39,8 @@ function CustomizeFrame() {
                 />
                 <div className='flex flex-col justify-center items-center border rounded-sm p-4 transition peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-sky-300 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
                     <img src='src/assets/product-image-without-pp.png' alt='White Frame' className='w-50 rounded my-5 mb-15' />
-                    <p className='text-neutral-900 mt-10'>White</p>
+                    <p className='text-neutral-900 text-xl mt-2 font-normal'>White</p>
+                    <p className='text-neutral-600 mt-10'>99kr</p>
                 </div>
             </label>
             <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
@@ -49,7 +52,8 @@ function CustomizeFrame() {
                 />
                 <div className='flex flex-col justify-center items-center border rounded-sm p-4 transition peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-sky-300 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
                     <img src='src/assets/product-image-without-pp.png' alt='Black Frame' className='w-50 rounded my-5 mb-15' />
-                    <p className='text-neutral-900 mt-10'>Black</p>
+                    <p className='text-neutral-900 text-xl mt-2 font-normal'>Black</p>
+                    <p className='text-neutral-600 mt-10'>99kr</p>
                 </div>
             </label>
         </div>
