e28b7e0de0c7212c9adf42ed26bb98dc4475dd1a
gitXite <daniel.halaas@gmail.com>
2025-08-26 09:14:18 +0200

    Styling the cards for customization

--- src/components/CustomizeFrame.tsx
+++ src/components/CustomizeFrame.tsx
@@ -3,53 +3,53 @@
 
 function CustomizeFrame() {
     return (
-        <div className='flex h-full w-1/3'>
-            <label className='flex flex-col items-center cursor-pointer'>
+        <div className='flex h-full w-full items-center justify-center'>
+            <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
                 <input 
                     type='radio'
                     name='frame'
                     value='oak'
                     className='peer hidden'
                 />
-                <div className='flex flex-col justify-center items-center border rounded-xl p-4 transition peer-checked:border-blue-500 peer-checked:ring-2 peer-checked:ring-blue-300 hover:scale-105 hover:shadow-lg'>
-                    <img src='' alt='Oak Frame' className='bg-neutral-900 h-20 w-20 rounded' />
-                    <p className='mt-2'>Oak Frame</p>
+                <div className='flex flex-col justify-center items-center border rounded-sm p-4 transition peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-sky-300 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                    <img src='src/assets/product-image-without-pp.png' alt='Oak Frame' className='w-50 rounded my-10' />
+                    <p className='text-neutral-900 mt-2'>Oak Frame</p>
                 </div>
             </label>
-            <label className='flex flex-col items-center cursor-pointer'>
+            <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
                 <input 
                     type='radio'
                     name='frame'
                     value='walnut'
                     className='peer hidden'
                 />
-                <div className='flex flex-col justify-center items-center border rounded-xl p-4 transition peer-checked:border-blue-500 peer-checked:ring-2 peer-checked:ring-blue-300 hover:scale-105 hover:shadow-lg'>
-                    <img src='' alt='Walnut Frame' className='bg-neutral-900 h-20 w-20 rounded' />
-                    <p className='mt-2'>Walnut Frame</p>
+                <div className='flex flex-col justify-center items-center border rounded-sm p-4 transition peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-sky-300 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                    <img src='src/assets/product-image-without-pp.png' alt='Walnut Frame' className='w-50 rounded my-10' />
+                    <p className='text-neutral-900 mt-2'>Walnut Frame</p>
                 </div>
             </label>
-            <label className='flex flex-col items-center cursor-pointer'>
+            <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
                 <input 
                     type='radio'
                     name='frame'
                     value='white'
                     className='peer hidden'
                 />
-                <div className='flex flex-col justify-center items-center border rounded-xl p-4 transition peer-checked:border-blue-500 peer-checked:ring-2 peer-checked:ring-blue-300 hover:scale-105 hover:shadow-lg'>
-                    <img src='' alt='White Frame' className='bg-neutral-900 h-20 w-20 rounded' />
-                    <p className='mt-2'>White Frame</p>
+                <div className='flex flex-col justify-center items-center border rounded-sm p-4 transition peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-sky-300 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                    <img src='src/assets/product-image-without-pp.png' alt='White Frame' className='w-50 rounded my-10' />
+                    <p className='text-neutral-900 mt-2'>White Frame</p>
                 </div>
             </label>
-            <label className='flex flex-col items-center cursor-pointer'>
+            <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
                 <input 
                     type='radio'
                     name='frame'
                     value='black'
                     className='peer hidden'
                 />
-                <div className='flex flex-col justify-center items-center border rounded-xl p-4 transition peer-checked:border-blue-500 peer-checked:ring-2 peer-checked:ring-blue-300 hover:scale-105 hover:shadow-lg'>
-                    <img src='' alt='Black Frame' className='bg-neutral-900 h-20 w-20 rounded' />
-                    <p className='mt-2'>Black Frame</p>
+                <div className='flex flex-col justify-center items-center border rounded-sm p-4 transition peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-sky-300 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                    <img src='src/assets/product-image-without-pp.png' alt='Black Frame' className='w-50 rounded my-10' />
+                    <p className='text-neutral-900 mt-2'>Black Frame</p>
                 </div>
             </label>
         </div>
--- src/components/CustomizePassePartout.tsx
+++ src/components/CustomizePassePartout.tsx
@@ -3,41 +3,41 @@
 
 function CustomizePassePartout() {
     return (
-        <div className='flex h-full w-1/3'>
-            <label className='flex flex-col items-center cursor-pointer'>
+        <div className='flex h-full w-full justify-center items-center'>
+            <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
                 <input 
                     type='radio'
                     name='passe-partout'
                     value='without'
                     className='peer hidden'
                 />
-                <div className='flex flex-col justify-center items-center border rounded-xl p-4 transition peer-checked:border-blue-500 peer-checked:ring-2 peer-checked:ring-blue-300 hover:scale-105 hover:shadow-lg'>
-                    <img src='' alt='Without' className='bg-neutral-900 h-20 w-20 rounded' />
-                    <p className='mt-2'>Without</p>
+                <div className='flex flex-col justify-center items-center border rounded-sm p-4 transition peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-sky-300 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                    <img src='src/assets/product-image-without-pp.png' alt='Without' className='w-50 my-10 rounded' />
+                    <p className='text-neutral-900 mt-2'>Without</p>
                 </div>
             </label>
-            <label className='flex flex-col items-center cursor-pointer'>
+            <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
                 <input 
                     type='radio'
                     name='passe-partout'
                     value='white'
                     className='peer hidden'
                 />
-                <div className='flex flex-col justify-center items-center border rounded-xl p-4 transition peer-checked:border-blue-500 peer-checked:ring-2 peer-checked:ring-blue-300 hover:scale-105 hover:shadow-lg'>
-                    <img src='' alt='White' className='bg-neutral-900 h-20 w-20 rounded' />
-                    <p className='mt-2'>White</p>
+                <div className='flex flex-col justify-center items-center border rounded-sm p-4 transition peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-sky-300 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                    <img src='src/assets/product-image-without-pp.png' alt='White' className='w-50 my-10 rounded' />
+                    <p className='text-neutral-900 mt-2'>White</p>
                 </div>
             </label>
-            <label className='flex flex-col items-center cursor-pointer'>
+            <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
                 <input 
                     type='radio'
                     name='passe-partout'
                     value='black'
                     className='peer hidden'
                 />
-                <div className='flex flex-col justify-center items-center border rounded-xl p-4 transition peer-checked:border-blue-500 peer-checked:ring-2 peer-checked:ring-blue-300 hover:scale-105 hover:shadow-lg'>
-                    <img src='' alt='Black' className='bg-neutral-900 h-20 w-20 rounded' />
-                    <p className='mt-2'>Black</p>
+                <div className='flex flex-col justify-center items-center border rounded-sm p-4 transition peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-sky-300 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                    <img src='src/assets/product-image-without-pp.png' alt='Black' className='w-50 my-10 rounded' />
+                    <p className='text-neutral-900 mt-2'>Black</p>
                 </div>
             </label>
         </div>
