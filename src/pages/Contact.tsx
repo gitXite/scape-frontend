b21a4c7b79c3b9e7d3b27205151389279299638a
gitXite <daniel.halaas@gmail.com>
2025-09-01 13:29:49 +0200

    Style contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -8,7 +8,10 @@ function Contact() {
         <div className='flex flex-col h-full w-full bg-neutral-100'>
             <div className='flex h-full w-full items-center py-10'>
                 <BackButton />
-                <div className='flex min-h-full w-full text-center justify-center items-center'>
+                {/* <div className='flex flex-col h-full w-2/4 text-neutral-900 text-6xl font-normal tracking-wide justfy-start items-center px-50'>
+                    <h1 className=''></h1>
+                </div> */}
+                <div className='flex h-full w-full text-center justify-center items-center'>
                     <ContactForm />
                 </div>
             </div>
