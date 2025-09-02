1beece8d6a923fbba1b161d0dac69745fe765b7c
gitXite <daniel.halaas@gmail.com>
2025-09-02 11:41:01 +0200

    Add Terms of Service

--- src/pages/TermsOfService.tsx
+++ src/pages/TermsOfService.tsx
@@ -1,11 +1,17 @@
-
-
+import BackButton from '@/components/BackButton';
 
 function TermsOfService() {
     return (
-        <div></div>
+        <div className='h-full w-full bg-neutral-100'>
+            <BackButton />
+            <iframe 
+                title='Terms of Service'
+                src='/termsofservice.html'
+                width='100%'
+                style={{ minHeight: '100vh' }}
+            />
+        </div>
     );
 }
-
 
 export default TermsOfService;
