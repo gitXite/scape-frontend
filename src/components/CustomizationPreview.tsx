6466e2aab2a656a81dce1949b8a27090ccc8789d
gitXite <daniel.halaas@gmail.com>
2025-09-04 16:09:42 +0200

    Create customization preview component

--- src/components/CustomizationPreview.tsx
+++ src/components/CustomizationPreview.tsx
@@ -1,13 +1,18 @@
-
+import Frame_Oak from '@/assets/frame_oak.avif';
+import Frame_Dark_Oak from '@/assets/frame_darkoak.avif';
+import Frame_White from '@/assets/frame_white.avif';
+import Frame_Black from '@/assets/frame_black.webp';
+import PassePartout_White from '@/assets/Passepartout_White.webp';
+import PassePartout_Black from '@/assets/Passepartout_Black.webp';
 
 
 function CustomizationPreview() {
     return (
-        <div className='h-full w-2/3 place-content-center place-items-center'>
-            <div className='h-[80%] w-full p-10'>
-                <img src='' alt='Preview Frame' className='z-3 w-auto' />
-                <img src='' alt='Preview Passe-Partout' className='z-2 w-auto' />
-                <img src='' alt='Preview Model' className='z-1 w-auto' />
+        <div className='min-h-full w-full'>
+            <div className='flex justify-center p-10'>
+                <img src={Frame_Dark_Oak} alt='Preview Frame' className='z-3 w-[40%] absolute' />
+                <img src={PassePartout_White} alt='Preview Passe-Partout' className='z-2 w-[40%] absolute' />
+                {/* <img src='' alt='Preview Model' className='z-1 w-2/4 absolute' /> */}
             </div>
         </div>
     );
