a6959c4c6106379040fbdfd38892f0efacb807d9
gitXite <daniel.halaas@gmail.com>
2025-09-04 16:30:28 +0200

    Fix props to render the chosen items

--- src/components/CustomizationPreview.tsx
+++ src/components/CustomizationPreview.tsx
@@ -6,12 +6,26 @@ import PassePartout_White from '@/assets/Passepartout_White.webp';
 import PassePartout_Black from '@/assets/Passepartout_Black.webp';
 
 
-function CustomizationPreview() {
+type CustomizationPreviewProps = {
+    frameType: 'oak' | 'walnut' | 'white' | 'black';
+    passePartoutType: 'without' | 'white' | 'black';
+};
+
+
+function CustomizationPreview({ frameType, passePartoutType }: CustomizationPreviewProps) {
     return (
         <div className='min-h-full w-full'>
             <div className='flex justify-center p-10'>
-                <img src={Frame_Dark_Oak} alt='Preview Frame' className='z-3 w-[40%] absolute' />
-                <img src={PassePartout_White} alt='Preview Passe-Partout' className='z-2 w-[40%] absolute' />
+                <img
+                    src={frameType}
+                    alt='Preview Frame'
+                    className='z-3 w-[40%] absolute'
+                />
+                <img
+                    src={passePartoutType}
+                    alt='Preview Passe-Partout'
+                    className='z-2 w-[40%] absolute'
+                />
                 {/* <img src='' alt='Preview Model' className='z-1 w-2/4 absolute' /> */}
             </div>
         </div>
