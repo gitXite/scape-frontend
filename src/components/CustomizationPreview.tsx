3c8249f640c72df9bedf01aa7133eeca2ffbe081
Daniel <Daniel.halaas@gmail.com>
2025-09-04 11:24:55 +0200

    Update CustomizationPreview.tsx

--- src/components/CustomizationPreview.tsx
+++ src/components/CustomizationPreview.tsx
@@ -4,8 +4,10 @@
 function CustomizationPreview() {
     return (
         <div className='h-full w-2/3 place-content-center place-items-center'>
-            <div className='h-[80%] p-10'>
-                <img src='' alt='Preview' className='bg-neutral-900' />
+            <div className='h-[80%] w-full p-10'>
+                <img src='' alt='Preview Frame' className='z-3 w-auto' />
+                <img src='' alt='Preview Passe-Partout' className='z-2 w-auto' />
+                <img src='' alt='Preview Model' className='z-1 w-auto' />
             </div>
         </div>
     );
