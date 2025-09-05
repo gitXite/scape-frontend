c1cc9a077d7fdaf08ac4faa050890697ddd99b1b
gitXite <daniel.halaas@gmail.com>
2025-09-05 15:15:56 +0200

    Fix the passe partout sizes

--- src/components/CustomizePassePartout.tsx
+++ src/components/CustomizePassePartout.tsx
@@ -2,6 +2,8 @@ import { useCustomization } from '@/context/CustomizationContext';
 import CustomizationPreview from './CustomizationPreview';
 import { Separator } from './ui/separator';
 import React from 'react';
+import Passepartout_White from '@/assets/Passepartout_White.webp';
+import Passepartout_Black from '@/assets/Passepartout_Black.webp';
 
 function CustomizePassePartout() {
     const { passePartoutType, setPassePartoutType } = useCustomization();
@@ -15,38 +17,10 @@ function CustomizePassePartout() {
 
     return (
         <div className='flex h-full w-full'>
-            <div className='flex h-full w-1/3 justify-center items-center pb-20'>
+            <div className='flex h-full w-2/4'>
                 <CustomizationPreview />
             </div>
-            <div className='flex h-full w-2/3 justify-center items-center pb-20'>
-                <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
-                    <input
-                        type='radio'
-                        name='passe-partout'
-                        value='without'
-                        checked={passePartoutType === 'without'}
-                        onChange={handleClick}
-                        className='peer hidden'
-                    />
-                    <div className='flex flex-col min-h-80 min-w-60 text-center justify-center items-center bg-neutral-200/20 hover:bg-neutral-100 peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
-                        <img
-                            src='src/assets/product-image-without-pp.png'
-                            alt='Without'
-                            className='w-50 mb-2 rounded'
-                        />
-                        <p className='text-neutral-500 w-60 pt-10 px-2'>
-                            Without a passe-partout -
-                        </p>
-                        <p className='text-neutral-500 w-60 pb-10 px-2'>
-                            Pure focus on the model
-                        </p>
-                        <p className='text-neutral-900 text-xl mt-2 pb-4 font-normal'>
-                            Without
-                        </p>
-                        <Separator orientation='horizontal' />
-                        <p className='text-neutral-600 pt-4'>19kr</p>
-                    </div>
-                </label>
+            <div className='flex h-full w-2/4 justify-center items-center pr-100'>
                 <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
                     <input
                         type='radio'
@@ -56,23 +30,23 @@ function CustomizePassePartout() {
                         onChange={handleClick}
                         className='peer hidden'
                     />
-                    <div className='flex flex-col min-h-80 min-w-60 text-center justify-center items-center bg-neutral-200/20 hover:bg-neutral-100 peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                    <div className='flex flex-col max-h-80 max-w-60 text-center justify-center items-center bg-neutral-200/20 hover:bg-neutral-100 peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
                         <img
-                            src='src/assets/product-image-without-pp.png'
+                            src={Passepartout_White}
                             alt='White'
-                            className='w-50 mb-2 rounded'
+                            className='w-40 mb-2 rounded'
                         />
-                        <p className='text-neutral-500 w-60 pt-10 px-2'>
+                        <p className='text-neutral-500 w-60 px-2'>
                             Enclosed by white -
                         </p>
-                        <p className='text-neutral-500 w-60 pb-10 px-2'>
+                        <p className='text-neutral-500 w-60 pb-5 px-2'>
                             Bright, airy, and classic
                         </p>
-                        <p className='text-neutral-900 text-xl mt-2 pb-4 font-normal'>
+                        <p className='text-neutral-900 text-xl pb-2 font-normal'>
                             White
                         </p>
                         <Separator orientation='horizontal' />
-                        <p className='text-neutral-600 pt-4'>49kr</p>
+                        <p className='text-neutral-600 pt-2'>49kr</p>
                     </div>
                 </label>
                 <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
@@ -84,23 +58,51 @@ function CustomizePassePartout() {
                         onChange={handleClick}
                         className='peer hidden'
                     />
-                    <div className='flex flex-col min-h-80 min-w-60 text-center justify-start items-center bg-neutral-200/20 hover:bg-neutral-100 peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                    <div className='flex flex-col max-h-80 max-w-60 text-center justify-center items-center bg-neutral-200/20 hover:bg-neutral-100 peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
                         <img
-                            src='src/assets/product-image-without-pp.png'
+                            src={Passepartout_Black}
                             alt='Black'
-                            className='w-50 mb-2 rounded'
+                            className='w-40 mb-2 rounded'
                         />
-                        <p className='text-neutral-500 w-60 pt-10 px-2'>
+                        <p className='text-neutral-500 w-60 px-2'>
                             Enclosed by black -
                         </p>
-                        <p className='text-neutral-500 w-60 pb-10 px-2'>
+                        <p className='text-neutral-500 w-60 pb-5 px-2'>
                             Sharp contrast for a bold look
                         </p>
-                        <p className='text-neutral-900 text-xl mt-2 pb-4 font-normal'>
+                        <p className='text-neutral-900 text-xl pb-2 font-normal'>
                             Black
                         </p>
                         <Separator orientation='horizontal' />
-                        <p className='text-neutral-600 pt-4'>49kr</p>
+                        <p className='text-neutral-600 pt-2'>49kr</p>
+                    </div>
+                </label>
+                <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
+                    <input
+                        type='radio'
+                        name='passe-partout'
+                        value='without'
+                        checked={passePartoutType === 'without'}
+                        onChange={handleClick}
+                        className='peer hidden'
+                    />
+                    <div className='flex flex-col max-h-80 max-w-60 text-center justify-center items-center bg-neutral-200/20 hover:bg-neutral-100 peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                        {/* <img
+                            src=''
+                            alt='Without'
+                            className='w-40 mb-2 rounded'
+                        /> */}
+                        <p className='text-neutral-500 w-60 px-2'>
+                            Without a passe-partout -
+                        </p>
+                        <p className='text-neutral-500 w-60 pb-5 px-2'>
+                            Pure focus on the model
+                        </p>
+                        <p className='text-neutral-900 text-xl pb-2 font-normal'>
+                            Without
+                        </p>
+                        <Separator orientation='horizontal' />
+                        <p className='text-neutral-600 pt-2'>19kr</p>
                     </div>
                 </label>
             </div>
