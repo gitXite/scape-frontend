c9366ad5b1e00d8f315779a133c18c462ab747d0
gitXite <daniel.halaas@gmail.com>
2025-09-05 12:45:48 +0200

    Fix basic functionality for customization preview

--- src/components/CustomizationPreview.tsx
+++ src/components/CustomizationPreview.tsx
@@ -4,35 +4,71 @@ import Frame_White from '@/assets/frame_white.avif';
 import Frame_Black from '@/assets/frame_black.webp';
 import PassePartout_White from '@/assets/Passepartout_White.webp';
 import PassePartout_Black from '@/assets/Passepartout_Black.webp';
+import { useEffect, useState } from 'react';
 
 
 type CustomizationPreviewProps = {
-    frameType?: 'oak' | 'walnut' | 'white' | 'black';
-    passePartoutType?: 'without' | 'white' | 'black';
+    frameType?: string;
+    passePartoutType?: string;
 };
 
 
-function CustomizationPreview({ frameType, passePartoutType }: CustomizationPreviewProps) {
-    
+function CustomizationPreview({
+    frameType,
+    passePartoutType,
+}: CustomizationPreviewProps) {
+    const [frame, setFrame] = useState<string>('');
+    const [passePartout, setPassePartout] = useState<string>('');
+
+    useEffect(() => {
+        switch (frameType) {
+            case 'oak':
+                setFrame(Frame_Oak);
+                break;
+            case 'walnut':
+                setFrame(Frame_Dark_Oak);
+                break;
+            case 'white':
+                setFrame(Frame_White);
+                break;
+            case 'black':
+                setFrame(Frame_Black);
+                break;
+        }
+        switch (passePartoutType) {
+            case 'without':
+                setPassePartout('');
+                break;
+            case 'white':
+                setPassePartout(PassePartout_White);
+                break;
+            case 'black':
+                setPassePartout(PassePartout_Black);
+                break;
+        }
+    }, [frameType, passePartoutType]);
 
     return (
         <div className='min-h-full w-full'>
             <div className='flex justify-center p-10'>
-                <img
-                    src={Frame_Dark_Oak}
-                    alt='Preview Frame'
-                    className='z-3 w-[40%] absolute'
-                />
-                <img
-                    src={PassePartout_White}
-                    alt='Preview Passe-Partout'
-                    className='z-2 w-[40%] absolute'
-                />
+                {frameType && (
+                    <img
+                        src={frame}
+                        alt='Preview Frame'
+                        className='z-3 w-[40%] absolute'
+                    />
+                )}
+                {passePartoutType && (
+                    <img
+                        src={passePartout}
+                        alt='Preview Passe-Partout'
+                        className='z-2 w-[40%] absolute'
+                    />
+                )}
                 {/* <img src='' alt='Preview Model' className='z-1 w-2/4 absolute' /> */}
             </div>
         </div>
     );
 }
-
 
 export default CustomizationPreview;
--- src/components/CustomizeFrame.tsx
+++ src/components/CustomizeFrame.tsx
@@ -1,3 +1,4 @@
+import CustomizationPreview from './CustomizationPreview';
 import { Separator } from './ui/separator';
 import React, { useState } from 'react';
 
@@ -17,79 +18,84 @@ function CustomizeFrame() {
     };
     
     return (
-        <div className='flex h-full w-full items-center justify-center pb-20'>
-            <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
-                <input 
-                    type='radio'
-                    name='frame'
-                    value='oak'
-                    checked={selectedValue === 'oak'}
-                    onClick={handleClick}
-                    className='peer hidden'
-                />
-                <div className='flex flex-col min-h-80 min-w-60 text-center justify-center bg-neutral-200/20 hover:bg-neutral-100 items-center peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
-                    <img src='src/assets/product-image-without-pp.png' alt='Oak Frame' className='w-50 rounded mb-2' />
-                    <p className='text-neutral-500 w-60 pt-10 px-2'>Fitted with an oak frame -</p>
-                    <p className='text-neutral-500 w-60 pb-10 px-2'>Natural and timeless</p>
-                    <p className='text-neutral-900 text-xl mt-2 pb-4 font-normal'>Oak</p>
-                    <Separator orientation='horizontal' />
-                    <p className='text-neutral-600 pt-4'>99kr</p>
-                </div>
-            </label>
-            <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
-                <input 
-                    type='radio'
-                    name='frame'
-                    value='walnut'
-                    checked={selectedValue === 'walnut'}
-                    onClick={handleClick}
-                    className='peer hidden'
-                />
-                <div className='flex flex-col min-h-80 min-w-60 text-center justify-center items-center bg-neutral-200/20 hover:bg-neutral-100 peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
-                    <img src='src/assets/product-image-without-pp.png' alt='Walnut Frame' className='w-50 rounded mb-2' />
-                    <p className='text-neutral-500 w-60 pt-10 px-2'>Fitted with a walnut frame -</p>
-                    <p className='text-neutral-500 w-60 pb-10 px-2'>Rich and elegant</p>
-                    <p className='text-neutral-900 text-xl mt-2 pb-4 font-normal'>Walnut</p>
-                    <Separator orientation='horizontal' />
-                    <p className='text-neutral-600 pt-4'>99kr</p>
-                </div>
-            </label>
-            <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
-                <input 
-                    type='radio'
-                    name='frame'
-                    value='white'
-                    checked={selectedValue === 'white'}
-                    onClick={handleClick}
-                    className='peer hidden'
-                />
-                <div className='flex flex-col min-h-80 min-w-60 text-center justify-center items-center bg-neutral-200/20 hover:bg-neutral-100 peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
-                    <img src='src/assets/product-image-without-pp.png' alt='White Frame' className='w-50 rounded mb-2' />
-                    <p className='text-neutral-500 w-60 pt-10 px-2'>Fitted with a white frame -</p>
-                    <p className='text-neutral-500 w-60 pb-10 px-2'>Clean and minimal</p>
-                    <p className='text-neutral-900 text-xl mt-2 pb-4 font-normal'>White</p>
-                    <Separator orientation='horizontal' />
-                    <p className='text-neutral-600 pt-4'>99kr</p>
-                </div>
-            </label>
-            <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
-                <input 
-                    type='radio'
-                    name='frame'
-                    value='black'
-                    checked={selectedValue === 'black'}
-                    onClick={handleClick}
-                    className='peer hidden'
-                />
-                <div className='flex flex-col min-h-80 min-w-60 text-center justify-center items-center bg-neutral-200/20 hover:bg-neutral-100 peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
-                    <img src='src/assets/product-image-without-pp.png' alt='Black Frame' className='w-50 rounded mb-2' />
-                    <p className='text-neutral-500 w-60 pt-10 px-2'>Fitted with a black frame -</p>
-                    <p className='text-neutral-500 w-60 pb-10 px-2'>Bold and classic</p>
-                    <p className='text-neutral-900 text-xl mt-2 pb-4 font-normal'>Black</p>
-                    <Separator orientation='horizontal' />
-                    <p className='text-neutral-600 pt-4'>99kr</p>
-                </div>
-            </label>
+        <div className='flex h-full w-full'>
+            <div className='flex h-full w-1/3 items-center justify-center pb-20'>
+                <CustomizationPreview frameType={selectedValue} passePartoutType='' />
+            </div>
+            <div className='flex h-full w-2/3 items-center justify-center pb-20'>
+                <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
+                    <input 
+                        type='radio'
+                        name='frame'
+                        value='oak'
+                        checked={selectedValue === 'oak'}
+                        onClick={handleClick}
+                        className='peer hidden'
+                    />
+                    <div className='flex flex-col min-h-80 min-w-60 text-center justify-center bg-neutral-200/20 hover:bg-neutral-100 items-center peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                        <img src='src/assets/product-image-without-pp.png' alt='Oak Frame' className='w-50 rounded mb-2' />
+                        <p className='text-neutral-500 w-60 pt-10 px-2'>Fitted with an oak frame -</p>
+                        <p className='text-neutral-500 w-60 pb-10 px-2'>Natural and timeless</p>
+                        <p className='text-neutral-900 text-xl mt-2 pb-4 font-normal'>Oak</p>
+                        <Separator orientation='horizontal' />
+                        <p className='text-neutral-600 pt-4'>99kr</p>
+                    </div>
+                </label>
+                <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
+                    <input 
+                        type='radio'
+                        name='frame'
+                        value='walnut'
+                        checked={selectedValue === 'walnut'}
+                        onClick={handleClick}
+                        className='peer hidden'
+                    />
+                    <div className='flex flex-col min-h-80 min-w-60 text-center justify-center items-center bg-neutral-200/20 hover:bg-neutral-100 peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                        <img src='src/assets/product-image-without-pp.png' alt='Walnut Frame' className='w-50 rounded mb-2' />
+                        <p className='text-neutral-500 w-60 pt-10 px-2'>Fitted with a walnut frame -</p>
+                        <p className='text-neutral-500 w-60 pb-10 px-2'>Rich and elegant</p>
+                        <p className='text-neutral-900 text-xl mt-2 pb-4 font-normal'>Walnut</p>
+                        <Separator orientation='horizontal' />
+                        <p className='text-neutral-600 pt-4'>99kr</p>
+                    </div>
+                </label>
+                <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
+                    <input 
+                        type='radio'
+                        name='frame'
+                        value='white'
+                        checked={selectedValue === 'white'}
+                        onClick={handleClick}
+                        className='peer hidden'
+                    />
+                    <div className='flex flex-col min-h-80 min-w-60 text-center justify-center items-center bg-neutral-200/20 hover:bg-neutral-100 peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                        <img src='src/assets/product-image-without-pp.png' alt='White Frame' className='w-50 rounded mb-2' />
+                        <p className='text-neutral-500 w-60 pt-10 px-2'>Fitted with a white frame -</p>
+                        <p className='text-neutral-500 w-60 pb-10 px-2'>Clean and minimal</p>
+                        <p className='text-neutral-900 text-xl mt-2 pb-4 font-normal'>White</p>
+                        <Separator orientation='horizontal' />
+                        <p className='text-neutral-600 pt-4'>99kr</p>
+                    </div>
+                </label>
+                <label className='flex flex-col h-fit items-center cursor-pointer mx-5'>
+                    <input 
+                        type='radio'
+                        name='frame'
+                        value='black'
+                        checked={selectedValue === 'black'}
+                        onClick={handleClick}
+                        className='peer hidden'
+                    />
+                    <div className='flex flex-col min-h-80 min-w-60 text-center justify-center items-center bg-neutral-200/20 hover:bg-neutral-100 peer-checked:bg-neutral-100 border rounded-sm p-4 transition peer-checked:border-neutral-900 peer-checked:scale-105 peer-checked:shadow-lg hover:scale-105 hover:shadow-lg'>
+                        <img src='src/assets/product-image-without-pp.png' alt='Black Frame' className='w-50 rounded mb-2' />
+                        <p className='text-neutral-500 w-60 pt-10 px-2'>Fitted with a black frame -</p>
+                        <p className='text-neutral-500 w-60 pb-10 px-2'>Bold and classic</p>
+                        <p className='text-neutral-900 text-xl mt-2 pb-4 font-normal'>Black</p>
+                        <Separator orientation='horizontal' />
+                        <p className='text-neutral-600 pt-4'>99kr</p>
+                    </div>
+                </label>
+            </div>
         </div>
     );
 }
