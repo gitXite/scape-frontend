8da6dba76dddd3386f01868cde7327f0b4a975a6
gitXite <daniel.halaas@gmail.com>
2025-09-10 09:09:11 +0200

    Added a spinner

--- src/components/modals/ModelPreview.tsx
+++ src/components/modals/ModelPreview.tsx
@@ -1,6 +1,7 @@
 import { useHotkeys } from 'react-hotkeys-hook';
 import { X } from 'lucide-react';
 import type React from 'react';
+import { Spinner } from '../ui/shadcn-io/spinner/spinner';
 
 
 type ModelPreviewProps = {
@@ -16,6 +17,7 @@ function ModelPreview({ showModal, setShowModal }: ModelPreviewProps) {
 
     return (
         <div className='absolute h-6/7 w-3/4 -translate-y-10 bg-neutral-900 rounded-sm z-50 shadow-[inset_0px_0px_20px_rgba(0,0,0,0.6)]'>
+            <Spinner variant='circle' size={42} className='relative justify-self-center top-2/4 -translate-y-2/4' />
             <button 
                 onClick={() => setShowModal(false)}
                 className='absolute group top-0 right-0 p-2 px-5 hover:shadow-[inset_0px_0px_10px_rgba(0,0,0,0.5)] active:shadow-[inset_0px_0px_20px_rgba(0,0,0,0.6)] hover:bg-neutral-100 rounded-sm rounded-tl-none rounded-br-none items-center content-center justify-center transition-all duration-100'
