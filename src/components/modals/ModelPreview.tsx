bd06475aab2424b6e42ce11ab59180ae922bfd06
gitXite <daniel.halaas@gmail.com>
2025-11-03 10:26:05 +0100

    Change styling for close modal button

--- src/components/modals/ModelPreview.tsx
+++ src/components/modals/ModelPreview.tsx
@@ -125,7 +125,7 @@ function ModelPreview({ showModal, setShowModal, className }: ModelPreviewProps)
                 )}
                 <button 
                     onClick={() => setShowModal(false)}
-                    className='absolute group top-0 right-0 p-2 px-5 hover:shadow-[inset_0px_0px_10px_rgba(0,0,0,0.5)] active:shadow-[inset_0px_0px_20px_rgba(0,0,0,0.6)] hover:bg-neutral-100 rounded-sm rounded-tl-none rounded-br-none items-center content-center justify-center transition-all duration-100'
+                    className='absolute group top-0 right-0 p-2 px-5 hover:shadow-[inset_0px_0px_4px_rgba(0,0,0,0.4)] active:shadow-[inset_0px_0px_10px_rgba(0,0,0,0.4)] hover:bg-neutral-300 rounded-sm rounded-tl-none rounded-br-none items-center content-center justify-center transition-all duration-100'
                 >
                     <X size={25} className='text-neutral-300 group-hover:text-neutral-900 transition-all duration-100' />
                 </button>
