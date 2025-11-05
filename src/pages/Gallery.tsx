136b00b277e386b48ddeab6aa0704891ae0b005e
Daniel <Daniel.halaas@gmail.com>
2025-11-05 17:28:56 +0100

    Refactor image source to use imported asset

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,5 +1,6 @@
 import BackButton from '@/components/BackButton';
 import Footer from '@/components/Footer';
+import productStraight from '@/assets/product-image-straight.png';
 
 
 function Gallery() {
@@ -9,49 +10,49 @@ function Gallery() {
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-10 p-20 px-30 min-h-full w-full bg-neutral-100'>
                 <div>
                     <img
-                        src='src/assets/product-image-straight.png'
+                        src={productStraight}
                         className='rounded-sm shadow-md hover:scale-105 transition-transform duration-300'
                     />
                 </div>
                 <div>
                     <img
-                        src='src/assets/product-image-straight.png'
+                        src={productStraight}
                         className='rounded-sm shadow-md hover:scale-105 transition-transform duration-300'
                     />
                 </div>
                 <div>
                     <img
-                        src='src/assets/product-image-straight.png'
+                        src={productStraight}
                         className='rounded-sm shadow-md hover:scale-105 transition-transform duration-300'
                     />
                 </div>
                 <div>
                     <img
-                        src='src/assets/product-image-straight.png'
+                        src={productStraight}
                         className='rounded-sm shadow-md hover:scale-105 transition-transform duration-300'
                     />
                 </div>
                 <div>
                     <img
-                        src='src/assets/product-image-straight.png'
+                        src={productStraight}
                         className='rounded-sm shadow-md hover:scale-105 transition-transform duration-300'
                     />
                 </div>
                 <div>
                     <img
-                        src='src/assets/product-image-straight.png'
+                        src={productStraight}
                         className='rounded-sm shadow-md hover:scale-105 transition-transform duration-300'
                     />
                 </div>
                 <div>
                     <img
-                        src='src/assets/product-image-straight.png'
+                        src={productStraight}
                         className='rounded-sm shadow-md hover:scale-105 transition-transform duration-300'
                     />
                 </div>
                 <div>
                     <img
-                        src='src/assets/product-image-straight.png'
+                        src={productStraight}
                         className='rounded-sm shadow-md hover:scale-105 transition-transform duration-300'
                     />
                 </div>
