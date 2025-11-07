2242992f4f451912499c0b605a5bdd80e0b581e3
gitXite <daniel.halaas@gmail.com>
2025-11-07 18:39:51 +0100

    Change text size

--- src/components/Testemonials.tsx
+++ src/components/Testemonials.tsx
@@ -12,11 +12,11 @@ function Testemonials() {
     // get reviews from backend to display
 
     return (
-        <div className='min-h-full w-full'>
+        <div className='min-h-full w-full p-5'>
             <div className='flex flex-col h-full items-center justify-evenly'>
                 <div className='rounded-full bg-neutral-900 px-5 py-1.5 drop-shadow-lg'>
                     <StarRating readonly rating={averageRating} showValue size='lg' />
-                    <p className='text-neutral-200 tracking-wide text-center font-normal text-lg'>Total reviews: {totalReviews}</p>
+                    <p className='text-neutral-200 tracking-wide text-center font-normal text-lg'>Total reviews: <small>{totalReviews}</small></p>
                 </div>
                 <h1 className='text-neutral-900 text-center font-medium text-5xl tracking-wide text-shadow-md'>Words of praise from <br />our customers</h1>
                 <div className='flex flex-col gap-10 w-1/2'>
