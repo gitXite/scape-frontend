ba86ff954d37677f04a5169a836828307c293c52
gitXite <daniel.halaas@gmail.com>
2025-11-07 18:20:34 +0100

    Style Testimonials with marquee

---
+++ src/components/ReviewCard.tsx
@@ -0,0 +1,11 @@
+
+
+
+function ReviewCard() {
+    return (
+        <div className='h-50 w-60 bg-neutral-200/40 rounded-lg border-1 border-neutral-300 drop-shadow-md mx-10'></div>
+    );
+}
+
+
+export default ReviewCard;
--- src/components/Testemonials.tsx
+++ src/components/Testemonials.tsx
@@ -1,9 +1,48 @@
+import ReviewCard from './ReviewCard';
+import { Marquee, MarqueeContent, MarqueeFade } from './ui/shadcn-io/marquee/Marquee';
+import StarRating from './ui/starRating';
+
 
 
 function Testemonials() {
+    // mock variables
+    let averageRating = 4.2;
+    let totalReviews = 127;
+
     // get reviews from backend to display
+
     return (
-        <div></div>
+        <div className='min-h-full w-full'>
+            <div className='flex flex-col h-full items-center justify-evenly'>
+                <div className='rounded-full bg-neutral-900 px-5 py-1.5 drop-shadow-lg'>
+                    <StarRating readonly rating={averageRating} showValue size='lg' />
+                    <p className='text-neutral-200 tracking-wide text-center font-normal text-lg'>Total reviews: {totalReviews}</p>
+                </div>
+                <h1 className='text-neutral-900 text-center font-medium text-5xl tracking-wide text-shadow-md'>Words of praise from <br />our customers</h1>
+                <div className='flex flex-col gap-10 w-1/2'>
+                    <Marquee>
+                        <MarqueeFade side='left' />
+                        <MarqueeFade side='right' />
+                        <MarqueeContent speed={100} className='py-2'>
+                            <ReviewCard />
+                            <ReviewCard />
+                            <ReviewCard />
+                            <ReviewCard />
+                        </MarqueeContent>
+                    </Marquee>
+                    <Marquee>
+                        <MarqueeFade side='left' />
+                        <MarqueeFade side='right' />
+                        <MarqueeContent speed={100} direction='right' className='py-2'>
+                            <ReviewCard />
+                            <ReviewCard />
+                            <ReviewCard />
+                            <ReviewCard />
+                        </MarqueeContent>
+                    </Marquee>
+                </div>
+            </div>
+        </div>
     );
 }
 
