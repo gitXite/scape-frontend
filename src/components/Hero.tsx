c082dfc743c1c2251f4365b336fea361974fe3d3
gitXite <daniel.halaas@gmail.com>
2025-11-12 12:39:40 +0100

    redesign hero section, interactive 3D instead of video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router';
-import VideoBackground from './VideoBackground';
 import { ChevronDown } from 'lucide-react';
+import Hero3D from './Hero3D';
 
 
 function Hero() {
@@ -8,25 +8,26 @@ function Hero() {
 
     return (
         <div id='home' className='h-full w-full place-content-center'>
-            <div className='flex flex-col place-items-center place-content-center h-8/12 w-full shadow-[inset_0px_0px_20px_rgba(0,0,0,0.6)] bg-neutral-900 drop-shadow-xl drop-shadow-neutral-500/60'>
-                <VideoBackground />
-                <h1 className='z-2 mt-10 text-9xl drop-shadow-black drop-shadow-2xl tracking-widest [text-shadow:0_4px_10px_rgba(0,0,0,0.6)]'>S C /\ P E</h1>
-                <div className='flex tracking-widest text-4xl z-2 drop-shadow-black drop-shadow-2xl mt-10 space-x-10 [text-shadow:0_2px_8px_rgba(0,0,0,1)]'>
+            <div className='flex flex-col place-items-center place-content-center h-full w-full shadow-[inset_0px_0px_20px_rgba(0,0,0,0.6)] bg-neutral-900 drop-shadow-xl drop-shadow-neutral-500/60'>
+                <Hero3D />
+
+                <h1 className='z-2 relative bottom-30 text-9xl drop-shadow-black drop-shadow-2xl tracking-widest [text-shadow:0_4px_10px_rgba(0,0,0,0.6)] pointer-events-none'>S C /\ P E</h1>
+                <div className='flex tracking-widest relative bottom-30 text-5xl z-2 drop-shadow-black drop-shadow-2xl mt-10 space-x-10 [text-shadow:0_2px_8px_rgba(0,0,0,1)] pointer-events-none'>
                     <h2 className=''>F r o m</h2>
                     <h2 className=''>M a p</h2>
                     <h2 className=''>T o</h2>
                     <h2 className=''>F r a m e</h2>
                 </div>
                 <button 
-                    className='z-2 flex mt-20 p-5 pl-12 pr-12 text-xl backdrop-blur-md text-neutral-100 border-1 transition duration-150 shadow-[inset_0px_0px_10px_rgba(0,0,0,0.6)] hover:shadow-[inset_0px_0px_15px_rgba(0,0,0,0.6)] active:shadow-[inset_0px_0px_10px_rgba(0,0,0,0.6)] shadow-neutral-950 border-neutral-100 rounded-full hover:drop-shadow-xl hover:cursor-pointer'
+                    className='z-2 flex mt-20 p-5 pl-12 pr-12 relative bottom-30 text-xl font-normal backdrop-blur-xs text-neutral-900 border-1 border-neutral-50 transition-all duration-150 rounded-full shadow-md hover:cursor-pointer'
                     onClick={() => {navigate('/get-started')}}
                 >
                     <p>G e t</p><span className='w-4'></span><p>S t a r t e d</p>
                 </button>
-            </div>
-            <div className='flex flex-col text-center items-center'>
-                <a href='#thescape' className='text-lg text-neutral-500 relative top-10 cursor-pointer font-normal hover:text-neutral-600 active:text-neutral-500 transition-colors duration-100 pb-5 z-1 peer'>Read More</a>
-                <ChevronDown className='text-neutral-500/50 relative top-5 cursor-pointer z-0 peer-hover:translate-y-2 peer-hover:text-neutral-600/60 transition-all duration-200 ease-out' />
+                <div className='flex flex-col text-center items-center absolute bottom-20'>
+                    <a href='#thescape' className='text-lg text-neutral-900 relative top-10 cursor-pointer font-normal active:text-neutral-500 transition-colors duration-100 pb-5 z-1 peer'>Read More</a>
+                    <ChevronDown className='text-neutral-900/80 relative top-5 cursor-pointer z-0 peer-hover:translate-y-2 transition-all duration-200 ease-out' />
+                </div>
             </div>
         </div>
     );
---
+++ src/components/Hero3D.tsx
@@ -0,0 +1,144 @@
+import * as THREE from 'three';
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
+import { useEffect, useRef } from 'react';
+
+function Hero3D() {
+    const mountRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (!mountRef.current) return;
+        const container = mountRef.current;
+
+        const width = mountRef.current.clientWidth;
+        const height = mountRef.current.clientHeight;
+
+        const scene = new THREE.Scene();
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d')!;
+        canvas.width = 32;
+        canvas.height = 512;
+
+        const gradient = context.createLinearGradient(0, 0, 0, 512);
+        gradient.addColorStop(0, '#171717');
+        gradient.addColorStop(1, '#000000');
+        context.fillStyle = gradient;
+        context.fillRect(0, 0, 32, 512);
+
+        // const texture = new THREE.CanvasTexture(canvas);
+        scene.background = null;
+
+        const camera = new THREE.PerspectiveCamera(
+            50,
+            mountRef.current.clientWidth / mountRef.current.clientHeight,
+            0.1,
+            5000
+        );
+
+        const renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            alpha: true,
+        });
+        renderer.setSize(width, height);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        mountRef.current!.innerHTML = '';
+        mountRef.current.appendChild(renderer.domElement);
+
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+        const dirLight = new THREE.DirectionalLight(0xffffff, 2);
+        dirLight.position.set(1, 1, 1);
+        scene.add(ambientLight, dirLight);
+
+        const loader = new STLLoader();
+        let mesh: THREE.Mesh;
+        loader.load(
+            '/models/terrain.stl',
+            (geometry) => {
+                geometry.computeBoundingBox();
+                const center = new THREE.Vector3();
+                geometry.boundingBox?.getCenter(center);
+                geometry.center();
+
+                const material = new THREE.MeshStandardMaterial({
+                    color: 0xffffff,
+                    
+                });
+                mesh = new THREE.Mesh(geometry, material);
+                mesh.rotation.x = -Math.PI / 2;
+                mesh.rotation.y = Math.PI / 2;
+                scene.add(mesh);
+
+                const size = new THREE.Vector3();
+                geometry.boundingBox?.getSize(size);
+                camera.position.z = 180;
+                camera.position.x = 0;
+
+                URL.revokeObjectURL('/models/terrain.stl');
+            },
+            undefined,
+            (error) => {
+                console.error('Error loading STL:', error);
+                URL.revokeObjectURL('/models/terrain.stl');
+            }
+        );
+
+        let targetRotationX = 0;
+        let targetRotationY = 0;
+        let targetRotationZ = 0;
+        let currentRotationX = 0;
+        let currentRotationY = 0;
+        let currentRotationZ = 0;
+
+        const onMouseMove = (e: MouseEvent) => {
+            const rect = container.getBoundingClientRect();
+            const mouseY = (e.clientX - rect.left) / rect.width - 0.5;
+            const mouseX = (e.clientY - rect.top) / rect.height - 0.5;
+
+            targetRotationX = mouseX * Math.PI * 0.05;
+            targetRotationY = mouseY * Math.PI * 0.1;
+            targetRotationZ = mouseY * Math.PI * 0.05;
+        };
+        container.addEventListener('mousemove', onMouseMove);
+
+        let frameID: number;
+        const animate = () => {
+            frameID = requestAnimationFrame(animate);
+
+            if (mesh) {
+                currentRotationX += (targetRotationX - currentRotationX) * 0.05;
+                currentRotationY += (targetRotationY - currentRotationY) * 0.05;
+                currentRotationZ += (targetRotationZ - currentRotationZ) * 0.05;
+                mesh.rotation.x = -Math.PI / 2.5 + currentRotationX;
+                mesh.rotation.y = Math.PI / 7 + currentRotationY;
+                mesh.rotation.z = -Math.PI / 5 + currentRotationZ;
+            }
+            renderer.render(scene, camera);
+        };
+        animate();
+
+        return () => {
+            container.removeEventListener("mousemove", onMouseMove);
+            cancelAnimationFrame(frameID);
+            renderer.dispose();
+
+            scene.traverse((obj) => {
+                if (obj instanceof THREE.Mesh) {
+                    obj.geometry.dispose();
+                    if (obj.material instanceof THREE.Material) {
+                        obj.material.dispose();
+                    }
+                }
+            });
+
+            if (
+                mountRef.current &&
+                renderer.domElement.parentNode === mountRef.current
+            ) {
+                mountRef.current.removeChild(renderer.domElement);
+            }
+        };
+    }, []);
+
+    return <div ref={mountRef} className='absolute top-0 left-0 h-full w-full object-cover' />;
+}
+
+export default Hero3D;
