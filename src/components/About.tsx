46adf034cc1e3cf72d33bbfefc7e7e9d02ebe95a
gitXite <daniel.halaas@gmail.com>
2025-11-13 21:05:15 +0100

    add about component in AboutUs page

---
+++ src/components/About.tsx
@@ -0,0 +1,17 @@
+import { useInView } from 'react-intersection-observer';
+
+
+const options = {
+    triggerOnce: false,
+    threshold: 0.1,
+};
+
+function About() {
+    return (
+        <div className='flex flex-col min-h-[200vh] w-full bg-neutral-100'>
+            
+        </div>
+    );
+}
+
+export default About;
--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,15 +1,14 @@
 import BackButton from '@/components/BackButton';
 import Footer from '@/components/Footer';
-
+import About from '@/components/About';
+import { useInView } from 'react-intersection-observer';
 
 
 function AboutUs() {
     return (
         <div className='flex flex-col h-full w-full bg-neutral-100'>
             <BackButton page='home' />
-            <div className='flex flex-col min-h-full w-full'>
-
-            </div>
+            <About />
             <Footer />
         </div>
     );
