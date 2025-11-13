f805109297bd52b63191d7976ce9c824f09247a9
gitXite <daniel.halaas@gmail.com>
2025-11-13 15:52:18 +0100

    change chevron color for back button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -11,13 +11,13 @@ function BackButton({ page }: BackButtonProps) {
 
     return (
         <button 
-            className='flex group items-center font-medium fixed m-10 top-0 left-0 mt-9 h-fit text-lg transition-colors duration-100 text-neutral-600 hover:text-neutral-950 hover:cursor-pointer active:text-neutral-600'
+            className='flex group items-center font-medium fixed m-10 top-0 left-0 mt-9 h-fit text-lg transition-colors duration-100 text-neutral-600 hover:text-neutral-900 hover:cursor-pointer active:text-neutral-600'
             onClick={() => {
                 if (page === 'home') navigate('/');
                 if (page === 'back') navigate(-1);
             }}
         >
-            <ChevronLeft className='group-hover:-translate-x-2 mt-[1px] transition-transform duration-200 text-neutral-500 group-hover:text-neutral-600 ease-out' />
+            <ChevronLeft className='group-hover:-translate-x-2 mt-[1px] transition-transform duration-200 text-neutral-600 group-hover:text-neutral-800 ease-out' />
             {page.charAt(0).toUpperCase() + page.slice(1)}
         </button>
     );
