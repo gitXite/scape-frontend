03153d988be1b69d72f33743c4fc804eb7d3bde5
gitXite <daniel.halaas@gmail.com>
2025-11-21 16:11:54 +0100

    add order functionality, send STL file on checkout, navigate to '/' and reset localStorage

--- src/components/CheckoutSummary.tsx
+++ src/components/CheckoutSummary.tsx
@@ -1,6 +1,7 @@
 import { Button } from './ui/button';
 import { Separator } from './ui/separator';
 import { useCustomization } from '@/context/CustomizationContext';
+import { toast } from 'sonner';
 
 import Frame_Oak from '@/assets/frame_oak.webp';
 import Frame_Dark_Oak from '@/assets/frame_darkoak.webp';
@@ -9,7 +10,9 @@ import Frame_Black from '@/assets/frame_black.webp';
 import PassePartout_White from '@/assets/Passepartout_White.webp';
 import PassePartout_Black from '@/assets/Passepartout_Black.webp';
 import modelImage from '@/assets/product-image-straight.png';
-
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { Spinner } from './ui/shadcn-io/spinner/spinner';
 
 const frameImages: Record<string, string> = {
     oak: Frame_Oak,
@@ -24,23 +27,82 @@ const passePartoutImages: Record<string, string> = {
     without: '',
 };
 
+interface Checkout {
+    lat: number;
+    lng: number;
+    verticalScale: number;
+    scale: number;
+}
 
 function CheckoutSummary() {
+    const navigate = useNavigate();
     const { frameType, passePartoutType } = useCustomization();
+    const [isLoading, setIsLoading] = useState(false);
 
     const frame = frameImages[frameType] ?? '';
     const passePartout = passePartoutImages[passePartoutType] ?? '';
-    
+
+    const coords = localStorage.getItem('coordinates');
+    const verticalScale = localStorage.getItem('verticalScale');
+    const boxSize = localStorage.getItem('boxSize');
+    const checkout: Checkout = {
+        lat: JSON.parse(coords!).north,
+        lng: JSON.parse(coords!).west,
+        verticalScale: [parseFloat(verticalScale!)][0],
+        scale: [parseInt(boxSize!)][0],
+    };
+
+    const sendSTL = async () => {
+        setIsLoading(true);
+        try {
+            const response = await fetch(
+                `${import.meta.env.VITE_APP_API_URL}/order/send`,
+                {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(checkout),
+                }
+            );
+            const data = await response.json();
+            setIsLoading(false);
+            toast.success(data.message, {
+                description: 'Check your email',
+            });
+
+            localStorage.removeItem('coordinates');
+            localStorage.removeItem('verticalScale');
+            localStorage.removeItem('boxSize');
+            localStorage.removeItem('selectedFrame');
+            localStorage.removeItem('selectedPassePartout');
+            localStorage.setItem('step', '0');
+            setTimeout(() => {
+                window.dispatchEvent(new Event('coordinates-updated'));
+                window.dispatchEvent(new Event('frame-removed'));
+                window.dispatchEvent(new Event('passe-partout-removed'));
+            });
+
+            navigate('/');
+        } catch (err) {
+            setIsLoading(false);
+            console.error('Error:', err);
+        }
+    };
+
     return (
         <div className='flex-col h-full w-2/4 p-20 pt-10 content-center text-center'>
             <div className='flex-col pb-5'>
-                <h1 className='relative bottom-5 pb-5 text-neutral-900 text-xl font-medium'>Order Summary</h1>
-                <Separator orientation='horizontal' className='relative bottom-5'/>
+                <h1 className='relative bottom-5 pb-5 text-neutral-900 text-xl font-medium'>
+                    Order Summary
+                </h1>
+                <Separator
+                    orientation='horizontal'
+                    className='relative bottom-5'
+                />
                 <div className='flex justify-between items-center py-5 font-normal tracking-wide'>
-                    <img 
-                        src={modelImage} 
-                        alt='Model' 
-                        className='w-50 rounded-sm object-cover' 
+                    <img
+                        src={modelImage}
+                        alt='Model'
+                        className='w-50 rounded-sm object-cover'
                     />
                     <h2 className='text-neutral-900'>Scape</h2>
                     <p className='text-neutral-600'>199kr</p>
@@ -48,17 +110,17 @@ function CheckoutSummary() {
                 <div className='flex justify-between items-center py-2 font-normal tracking-wide'>
                     <div className='flex h-60 w-40 justify-center items-center'>
                         {frameType && (
-                            <img 
-                                src={frame} 
-                                alt='Frame' 
-                                className='w-40 rounded-sm z-2 absolute' 
+                            <img
+                                src={frame}
+                                alt='Frame'
+                                className='w-40 rounded-sm z-2 absolute'
                             />
                         )}
                         {passePartoutType && (
-                            <img 
-                                src={passePartout} 
-                                alt='Passe Partout' 
-                                className='w-39 rounded-sm absolute' 
+                            <img
+                                src={passePartout}
+                                alt='Passe Partout'
+                                className='w-39 rounded-sm absolute'
                             />
                         )}
                     </div>
@@ -66,7 +128,7 @@ function CheckoutSummary() {
                     <p className='text-neutral-600'>148kr</p>
                 </div>
             </div>
-            <Separator orientation='horizontal'/>
+            <Separator orientation='horizontal' />
             <div className='flex-col pt-5 pb-5'>
                 <div className='flex justify-between pb-2 font-normal tracking-wide'>
                     <p className='text-neutral-900'>Subtotal</p>
@@ -81,15 +143,29 @@ function CheckoutSummary() {
                     <p className='text-neutral-600'>49kr</p>
                 </div>
             </div>
-            <Separator orientation='horizontal'/>
+            <Separator orientation='horizontal' />
             <div className='flex justify-between py-5 font-medium tracking-wide'>
                 <h1 className='text-neutral-900'>Total</h1>
                 <p className='text-neutral-900'>396kr</p>
             </div>
-            <Button className='h-12 w-3/9 rounded-full bg-neutral-900 border-1 border-neutral-300 hover:bg-neutral-200 hover:text-neutral-900 active:bg-white cursor-pointer'>Order Now</Button>
+            {isLoading ? (
+                <Button
+                    onClick={sendSTL}
+                    disabled
+                    className='h-12 w-3/9 rounded-full bg-neutral-900 border-1 border-neutral-300 hover:bg-neutral-200 hover:text-neutral-900 active:bg-white cursor-pointer'
+                >
+                    <Spinner variant={'ellipsis'} />
+                </Button>
+            ) : (
+                <Button
+                    onClick={sendSTL}
+                    className='h-12 w-3/9 rounded-full bg-neutral-900 border-1 border-neutral-300 hover:bg-neutral-200 hover:text-neutral-900 active:bg-white cursor-pointer'
+                >
+                    Order Now
+                </Button>
+            )}
         </div>
     );
 }
-
 
 export default CheckoutSummary;
