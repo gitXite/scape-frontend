5a929e1e8b263c692069d048ff6119a89a5b3744
gitXite <daniel.halaas@gmail.com>
2025-11-25 15:33:55 +0100

    work on responsiveness, fix small typos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,140 +3,140 @@ import Socials from './Socials';
 
 function Footer() {
     return (
-        <footer className='flex justify-center place-items-end min-h-50 max-h-50 w-full p-5 border-t-1 bg-neutral-900 overflow-hidden'>
-            <div className='flex absolute left-0 ml-20 h-40 text-center space-x-2'>
-                <div className='flex flex-col items-start px-6 place-content-around relative'>
-                    <h1 className='text-neutral-100 group transition duration-200 font-medium tracking-wide'>
-                        Company
-                    </h1>
-                    <a
-                        href='/'
-                        className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
-                    >
-                        Home
-                        <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
-                    </a>
-                    <a
-                        href='/about-us'
-                        className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
-                    >
-                        About Us
-                        <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
-                    </a>
-                    <a
-                        href='/get-started'
-                        className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
-                    >
-                        Get Started
-                        <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
-                    </a>
-                    <a
-                        href='/testimonials'
-                        className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
-                    >
-                        Testimonials
-                        <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
-                    </a>
-                </div>
-                <div className='flex items-center justify-center h-full'>
-                    <Separator
-                        orientation='vertical'
-                        className='border-neutral-100/10 max-h-20'
-                    />
-                </div>
-                <div className='flex flex-col items-start px-6 place-content-around'>
-                    <h1 className='text-neutral-100 group transition duration-200 font-medium tracking-wide'>
-                        Support
-                    </h1>
-                    <a
-                        href='/faq'
-                        className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
-                    >
-                        FAQ
-                        <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
-                    </a>
-                    <a
-                        href='/shipping'
-                        className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
-                    >
-                        Orders & Shipping
-                        <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
-                    </a>
-                    <a
-                        href='/contact-us'
-                        className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
-                    >
-                        Contact Support
-                        <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
-                    </a>
-                    <a
-                        href='/feedback'
-                        className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
-                    >
-                        Submit Feedback
-                        <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
-                    </a>
-                </div>
-                <div className='flex items-center justify-center h-full'>
-                    <Separator
-                        orientation='vertical'
-                        className='border-neutral-100/10 max-h-20'
-                    />
-                </div>
-                <div className='flex flex-col items-start px-6 place-content-around'>
-                    <h1 className='text-neutral-100 group transition duration-200 font-medium tracking-wide'>
-                        Legal
-                    </h1>
-                    <a
-                        href='/privacy-policy'
-                        className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
-                    >
-                        Privacy Policy
-                        <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
-                    </a>
-                    <a
-                        href='/terms-of-service'
-                        className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
-                    >
-                        Terms of Service
-                        <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
-                    </a>
-                    <a
-                        href='/returns'
-                        className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
-                    >
-                        Returns & Refunds
-                        <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
-                    </a>
-                    <a
-                        href='/cookies'
-                        className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
-                    >
-                        Cookie Policy
-                        <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
-                    </a>
-                </div>
-            </div>
-            <div className='flex flex-col space-y-2 text-center'>
+        <footer className='flex max-xl:flex-col max-xl:place-items-center justify-center place-items-end min-h-50 max-xl:max-h-100 max-h-50 w-full p-5 border-t-1 bg-neutral-900 overflow-hidden'>
+            <div className='flex flex-col space-y-2 text-center place-content-end'>
                 <div className='h-fit w-auto text-4xl text-center text-neutral-100 mb-5'>
                     <h1 className=''>S C /\ P E</h1>
                     <p className='text-sm pt-2 pb-1 tracking-widest'>by md</p>
+                </div>
+                <div className='flex absolute max-xl:static left-0 ml-20 max-[1440px]:ml-5 max-xl:ml-0 h-40 text-center space-x-2 justify-evenly'>
+                    <div className='flex flex-col items-start max-xl:items-center px-6 max-sm:px-2 place-content-around'>
+                        <h1 className='text-neutral-100 group transition duration-200 font-medium tracking-wide'>
+                            Company
+                        </h1>
+                        <a
+                            href='/'
+                            className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
+                        >
+                            Home
+                            <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
+                        </a>
+                        <a
+                            href='/about-us'
+                            className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
+                        >
+                            About Us
+                            <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
+                        </a>
+                        <a
+                            href='/get-started'
+                            className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
+                        >
+                            Get Started
+                            <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
+                        </a>
+                        <a
+                            href='/testimonials'
+                            className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
+                        >
+                            Testimonials
+                            <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
+                        </a>
+                    </div>
+                    <div className='flex items-center justify-center h-full'>
+                        <Separator
+                            orientation='vertical'
+                            className='border-neutral-100/10 max-h-20'
+                        />
+                    </div>
+                    <div className='flex flex-col items-start max-xl:items-center px-6 max-sm:px-2 place-content-around'>
+                        <h1 className='text-neutral-100 group transition duration-200 font-medium tracking-wide'>
+                            Support
+                        </h1>
+                        <a
+                            href='/faq'
+                            className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
+                        >
+                            FAQ
+                            <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
+                        </a>
+                        <a
+                            href='/shipping'
+                            className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
+                        >
+                            Orders & Shipping
+                            <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
+                        </a>
+                        <a
+                            href='/contact-us'
+                            className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
+                        >
+                            Contact Support
+                            <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
+                        </a>
+                        <a
+                            href='/feedback'
+                            className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
+                        >
+                            Submit Feedback
+                            <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
+                        </a>
+                    </div>
+                    <div className='flex items-center justify-center h-full'>
+                        <Separator
+                            orientation='vertical'
+                            className='border-neutral-100/10 max-h-20'
+                        />
+                    </div>
+                    <div className='flex flex-col items-start max-xl:items-center px-6 max-sm:px-2 place-content-around'>
+                        <h1 className='text-neutral-100 group transition duration-200 font-medium tracking-wide'>
+                            Legal
+                        </h1>
+                        <a
+                            href='/privacy-policy'
+                            className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
+                        >
+                            Privacy Policy
+                            <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
+                        </a>
+                        <a
+                            href='/terms-of-service'
+                            className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
+                        >
+                            Terms of Service
+                            <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
+                        </a>
+                        <a
+                            href='/returns'
+                            className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
+                        >
+                            Returns & Refunds
+                            <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
+                        </a>
+                        <a
+                            href='/cookies'
+                            className='text-neutral-300 group hover:text-white transition duration-200 text-sm'
+                        >
+                            Cookie Policy
+                            <span className='block max-w-0 group-hover:max-w-full transition-all duration-100 h-0.5 bg-neutral-300'></span>
+                        </a>
+                    </div>
+                </div>
+                <div className='flex absolute max-xl:static right-0 items-center'>
+                    <small className='relative right-20 top-2'>scapebymd@gmail.com</small>
+                    <Socials />
+                    <small className='relative left-2 top-2'>Powered by</small>
+                    <img
+                        className='max-w-20 mr-20 max-[1440px]:mr-5 max-xl:mr-0 p-0 relative bottom-[-9px]'
+                        src='/images/Vipps-Logo.wine.png'
+                        alt='Vipps Logo'
+                    />
                 </div>
                 <p className='text-xs'>Org nr 929 981 626</p>
                 <p className='text-xs'>
                     Copyright © {new Date().getFullYear()} by md design. All
                     Rights Reserved.
                 </p>
-            </div>
-            <div className='flex absolute right-0 items-center'>
-                <small className='relative right-20 top-2'>scapebymd@gmail.com</small>
-                <Socials />
-                <small className='relative left-2 top-2'>Powered by</small>
-                <img
-                    className='max-w-20 mr-20 p-0 relative bottom-[-9px]'
-                    src='/images/Vipps-Logo.wine.png'
-                    alt='Vipps Logo'
-                />
             </div>
         </footer>
     );
--- src/components/TheScape.tsx
+++ src/components/TheScape.tsx
@@ -37,7 +37,7 @@ function TheScape() {
     return (
         <div
             id='thescape'
-            className='grid grid-cols-2 max-lg:flex max-lg:flex-col gap-x-20 w-full bg-neutral-100 min-h-[200vh] px-10 py-10'
+            className='grid grid-cols-2 max-lg:flex max-lg:flex-col gap-x-20 w-full bg-neutral-100 min-h-[200vh] px-10 max-sm:px-5 py-10'
         >
             <div className='flex flex-col items-center gap-y-80 mt-20 max-lg:gap-y-40'>
                 <div
@@ -50,11 +50,11 @@ function TheScape() {
                         From Map To Frame
                     </h1>
                     <p className='w-8/12 max-sm:w-full text-center text-neutral-700 leading-8'>
-                        With just a few clicks, you can transform any map location into a stunning and fully detailed 3D model. 
+                        With just a few clicks, you can transform any map location within Norway into a stunning and fully detailed 3D model. 
                         Simply choose your area of interest and our service automatically creates a precise topographic cutout, 
                         capturing terrain contours, elevation data, and the unique geographic character of the landscape. 
-                        The result is a clear and visually compellign 3D repsesentation that is ready for planning, design, visualization, or display. 
-                        Whether you want to highlight a favourite place, prepare a project, or expolore terrain in a completely new way, our service makes complex spatial data effortless, 
+                        The result is a clear and visually compelling 3D repsesentation that is ready for planning, design, visualization, or display. 
+                        Whether you want to highlight a favourite place, prepare a project, or explore terrain in a completely new way, our service makes complex spatial data effortless, 
                         intuitive, and beautifully rendered. 
                     </p>
                 </div>
@@ -68,11 +68,11 @@ function TheScape() {
                         Made By MD
                     </h1>
                     <p className='w-8/12 max-sm:w-full text-center text-neutral-700 leading-8'>
-                        We are Maren and Daniel, a nwly engaged couple turning our shared creativity into a meaningful side project as we save up for our wedding. 
-                        Maren brings her background in architecture, an eye for design, and a passiuon for spatial experiences. 
-                        Daniel contributes his skuills as a data engineering and computer science student, 
+                        We are Maren and Daniel, a newly engaged couple turning our shared creativity into a meaningful side project as we save up for our wedding. 
+                        Maren brings her background in architecture, an eye for design, and a passion for spatial experiences. 
+                        Daniel contributes his skills as a data engineering and computer science student, 
                         combining techical insight with problem solving curiosity. 
-                        Together, we've created a smal but thoughtful venture that blends design, technology, and a love for crafting something special. 
+                        Together, we've created a small but thoughtful venture that blends design, technology, and a love for crafting something special. 
                     </p>
                 </div>
             </div>
