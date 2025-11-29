c68b5b0ce5f4b833158af759213c5699a811a26f
gitXite <daniel.halaas@gmail.com>
2025-11-29 19:45:54 +0100

    fix: adjust mobile model control params

--- src/components/modals/ModelPreview.tsx
+++ src/components/modals/ModelPreview.tsx
@@ -60,9 +60,9 @@ function ModelPreview({
         
         const controls = new TrackballControls(camera, renderer.domElement);
         controls.staticMoving = false;
-        controls.rotateSpeed = 10.0;
-        controls.dynamicDampingFactor = 0.2;
-        controls.panSpeed = 0.3;
+        controls.rotateSpeed = window.innerWidth > 640 ? 10.0 : 1.0;
+        controls.dynamicDampingFactor = window.innerWidth > 640 ? 0.2 : 0.1;
+        controls.panSpeed = window.innerWidth > 640 ? 0.3 : 0.05;
 
         const light = new THREE.DirectionalLight(0xffffff, 3);
         light.position.set(1, 1, 1);
