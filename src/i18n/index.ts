455aff0dc370330f543647da4d5747dd09d3871e
gitXite <daniel.halaas@gmail.com>
2025-11-29 18:59:59 +0100

    install i18next and language detector

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,17 +1,15 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
 import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
-    .use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         fallbackLng: "nb",
         supportedLngs: ["en", "nb"],
         ns: ["common", "home", "getStarted"],
-        defaultNs: "common",
+        defaultNS: "common",
         interpolation: {
             escapeValue: false
         },
@@ -20,7 +18,7 @@ i18n
         },
         detection: {
             order: ["querystring", "localStorage", "navigator"],
-            caches: ["localStorage],
+            caches: ["localStorage"]
         },
         react: {
             useSuspense: true
