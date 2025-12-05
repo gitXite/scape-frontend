c29c852ac41002f99de45ca7b2f610a62c0a1628
gitXite <daniel.halaas@gmail.com>
2025-12-05 15:51:23 +0100

    add stl refetching/recaching when refreshing page

--- src/pages/GetStarted.tsx
+++ src/pages/GetStarted.tsx
@@ -6,11 +6,16 @@ import CustomizeFrame from '../components/CustomizeFrame';
 import CustomizePassePartout from '../components/CustomizePassePartout';
 import { Stepper } from '@/components/ui/stepper';
 import { CircleQuestionMark } from 'lucide-react';
-import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hoverCard';
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from '@/components/ui/hoverCard';
 import { Separator } from '@/components/ui/separator';
 import { CustomizationProvider } from '@/context/CustomizationContext';
-
-
+import { STLCache } from '@/utils/cache';
+import { generateAndFetchSTL } from '@/utils/generateAndFetchSTL';
+import { parseSTL } from '@/utils/parseSTL';
 
 function GetStarted() {
     const [activeStep, setActiveStep] = useState(() => {
@@ -26,26 +31,43 @@ function GetStarted() {
 
     useEffect(() => {
         localStorage.setItem('step', activeStep.toString());
+        const refresh = async () => {
+            if (localStorage.getItem('coordinates') && !STLCache.objectUrl) {
+                await generateAndFetchSTL().then(stlObject => parseSTL(stlObject?.buffer!));
+            }
+        };
+        refresh();
     }, [activeStep]);
-    
+
     return (
         <div className='bg-neutral-100 h-svh w-full'>
             <BackButton page='home' />
             <HoverCard>
-                <HoverCardTrigger className='absolute right-10 top-9 max-md:hidden z-5'><CircleQuestionMark className='text-neutral-600/50 hover:text-neutral-900 transition-colors duration-200 cursor-pointer' /></HoverCardTrigger>
+                <HoverCardTrigger className='absolute right-10 top-9 max-md:hidden z-5'>
+                    <CircleQuestionMark className='text-neutral-600/50 hover:text-neutral-900 transition-colors duration-200 cursor-pointer' />
+                </HoverCardTrigger>
                 <HoverCardContent className='bg-neutral-900 text-neutral-100 text-center justify-center'>
                     <p>1. </p>
                     <b>Select your coordinates -</b>
                     <p className='pb-3'>Choose the exact location you want</p>
-                    <Separator orientation='horizontal' className='border-neutral-100/10' />
+                    <Separator
+                        orientation='horizontal'
+                        className='border-neutral-100/10'
+                    />
                     <p className='pt-2'>2. </p>
                     <b>Pick a frame -</b>
                     <p className='pb-3'>Find the style that fits your space</p>
-                    <Separator orientation='horizontal' className='border-neutral-100/10' />
+                    <Separator
+                        orientation='horizontal'
+                        className='border-neutral-100/10'
+                    />
                     <p className='pt-2'>3. </p>
                     <b>Add a passepartout -</b>
                     <p className='pb-3'>Give your model a refined finish</p>
-                    <Separator orientation='horizontal' className='border-neutral-100/10' />
+                    <Separator
+                        orientation='horizontal'
+                        className='border-neutral-100/10'
+                    />
                     <p className='pt-2'>4. </p>
                     <b>Checkout -</b>
                     <p>Get your customized piece delivered to you</p>
@@ -53,7 +75,11 @@ function GetStarted() {
             </HoverCard>
             <div className='flex h-full w-full place-items-center'>
                 <CustomizationProvider>
-                    <Stepper steps={steps} currentStep={activeStep} onStepChange={setActiveStep} />
+                    <Stepper
+                        steps={steps}
+                        currentStep={activeStep}
+                        onStepChange={setActiveStep}
+                    />
                 </CustomizationProvider>
             </div>
 
@@ -61,6 +87,5 @@ function GetStarted() {
         </div>
     );
 }
-
 
 export default GetStarted;
