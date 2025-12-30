5e691f5546680bc2519135816840afac92d4297a
gitXite <daniel.halaas@gmail.com>
2025-12-30 12:28:01 +0100

    slight change to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router';
-import { ChevronDown, ChevronRight } from 'lucide-react';
+import { ArrowRight, ChevronDown, /*ChevronRight*/ } from 'lucide-react';
 import Hero3D from './Hero3D';
 import { motion } from 'motion/react';
 
@@ -51,12 +51,23 @@ function Hero() {
                     animate='visible'
                 >
                     <div className='flex tracking-widest relative text-white bottom-50 text-5xl max-md:text-3xl max-sm:text-2xl z-2 drop-shadow-black drop-shadow-2xl mt-10 space-x-10 max-sm:space-x-5 [text-shadow:0_2px_8px_rgba(0,0,0,1)] pointer-events-none'>
-                        <h2 className=''>F r o m</h2>
-                        <h2 className=''>M a p</h2>
+                        <h2 className='tracking-normal'>From Map To Frame</h2>
+                        {/* <h2 className=''>M a p</h2>
                         <h2 className=''>T o</h2>
-                        <h2 className=''>F r a m e</h2>
+                        <h2 className=''>F r a m e</h2> */}
                     </div>
                 </motion.div>
+                {/* <motion.p
+                    custom={1}
+                    variants={fadeUpVariants}
+                    initial='hidden'
+                    animate='visible'
+                    className='max-w-lg [text-shadow:0_2px_8px_rgba(0,0,0,1)] text-center text-xl text-neutral-950 leading-relaxed font-thin z-10 pointer-events-none'
+                >
+                    Transform any Norwegian landscape into a stunning 3D
+                    topographic model. Precision-crafted artwork that captures
+                    the beauty of your favorite places.
+                </motion.p> */}
                 <div className='flex flex-col absolute bottom-20 max-sm:bottom-10'>
                     <motion.div
                         custom={2}
@@ -71,10 +82,10 @@ function Hero() {
                                 navigate('/get-started');
                             }}
                         >
-                            <p>G e t</p>
-                            <span className='w-4'></span>
-                            <p>S t a r t e d</p>
-                            <ChevronRight className='self-center relative max-sm:hidden left-4 top-[1px] p-0 m-0 group-hover:translate-x-2 transition-all duration-200 ease-out' />
+                            <p className='tracking-widest'>Get Started</p>
+                            {/* <span className='w-4'></span> */}
+                            {/* <p>Started</p> */}
+                            <ArrowRight className='self-center w-[18px] text-neutral-900 relative max-sm:hidden left-4 top-[1px] p-0 m-0 group-hover:translate-x-2 transition-all duration-200 ease-out' />
                         </button>
                     </motion.div>
                     <motion.div
@@ -87,11 +98,11 @@ function Hero() {
                         <div className='flex flex-col text-center items-center mt-5'>
                             <a
                                 href='#thescape'
-                                className='text-lg text-neutral-900 cursor-pointer font-normal active:text-neutral-700 transition-colors duration-100 z-1 peer'
+                                className='text-lg tracking-tight text-neutral-900 cursor-pointer font-normal active:text-neutral-700 transition-colors duration-100 z-1 peer'
                             >
-                                Read More
+                                Explore
                             </a>
-                            <ChevronDown className='text-neutral-900/85 pointer-events-none z-0 peer-hover:translate-y-2 transition-all duration-200 ease-out' />
+                            <ChevronDown className='text-neutral-900/85 pointer-events-none z-0 peer-hover:translate-y-2 relative top-2 transition-all duration-200 ease-out peer-hover:animate-none animate-bounce' />
                         </div>
                     </motion.div>
                 </div>
